705b8da459ba929629522aa9fb1698009872627f
Aidan Hilt <aidanhilt2@gmail.com>
2023-01-13 00:08:21 -0500

    Adding mac setup files, and starting on server.

---
+++ mac-setup/installs.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+#TODO add setup for Syncthing, and automatically update wallpapers
+
+echo 'source <(kubectl completion zsh)' >>~/.zshrc
+echo 'alias k=kubectl' >>~/.zshrc
+echo 'complete -F __start_kubectl ks' >>~/.zshrc
+
+
+
+
+
+
+source ~/.zshrc
