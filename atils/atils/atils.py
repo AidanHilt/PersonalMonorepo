c2f074494d2fb883dfabb37d3c229089b7b6c8dc
Aidan Hilt <11202897+AidanHilt@users.noreply.github.com>
2023-10-12 22:14:55 -0400

    Atils (#29)
    * Think I'm done for now
    
    * Huh?
    
    * Let's finish this off

--- atils/atils/atils.py
+++ atils/atils/atils.py
@@ -1,10 +1,8 @@
 import sys
 
-from atils import atils_kubernetes as kubernetes
-from atils import python
-from atils import kustomize
 from atils import argocd
-from atils import jobs
+from atils import atils_kubernetes as kubernetes
+from atils import build, jobs, kustomize, python
 
 
 def main():
@@ -26,6 +24,8 @@ def main():
     # Doing it this way because there's no reason to pull it all out of atils_kubernetes
     elif script_name == "argocd":
         argocd.main(sys.argv[2:])
+    elif script_name == "build":
+        build.main(sys.argv[2:])
     elif script_name == "job":
         jobs.main(sys.argv[2:])
     else:
