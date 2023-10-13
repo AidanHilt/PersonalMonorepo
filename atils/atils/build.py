2428d0c897d69ebf834dde403b427e78e20449f9
Aidan Hilt <aidanhilt2@gmail.com>
2023-10-12 22:14:01 -0400

    Let's finish this off

--- atils/atils/build.py
+++ atils/atils/build.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import logging
 import os
+import subprocess
 import sys
 
 from atils.common import config
@@ -28,17 +29,37 @@ def main(args: list[str]):
 
 
 def get_available_build_actions() -> list[object]:
-    filename = ".atils\_buildconfig.json"
+    filename = os.path.join(os.getcwd(), ".atils_buildconfig.json")
 
     if os.path.isfile(filename):
         with open(filename) as f:
             data = json.load(f)
-            return data
+            return sorted(data, key=lambda x: x["order"])
     else:
         raise FileNotFoundError(f"{filename} does not exist")
 
 
+def validate_listed_actions(available_actions: list[object], listed_actions: list[str]):
+    available_action_names = [action["name"] for action in available_actions]
+    for action in listed_actions:
+        if action not in available_action_names and action != "all":
+            raise ValueError(f"{action} is not a valid action")
+
+
+def run_action(action: object):
+    subprocess.run(action["command"], shell=True)
+
+
 def run_build_actions(actions):
-    print(get_available_build_actions())
-    for action in actions[0][1]:
-        print(action)
+    listed_actions = actions[0][1]
+    available_actions = get_available_build_actions()
+
+    validate_listed_actions(available_actions, listed_actions)
+
+    if "all" in listed_actions:
+        for action in available_actions:
+            run_action(action)
+    else:
+        for action in available_actions:
+            if action["name"] in listed_actions:
+                run_action(action)
