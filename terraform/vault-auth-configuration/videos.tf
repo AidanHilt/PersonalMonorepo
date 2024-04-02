4258f7f1c4f267aaf8f457aef1a1b337168c3d78
Aidan Hilt <aidanhilt2@gmail.com>
2024-04-01 23:08:17 -0400

    Let's do it

--- terraform/vault-auth-configuration/videos.tf
+++ terraform/vault-auth-configuration/videos.tf
@@ -16,4 +16,5 @@ resource "vault_kubernetes_auth_backend_role" "video_stack_reader" {
   bound_service_account_namespaces = ["videos"]
   token_ttl                        = 3600
   token_policies                   = [vault_policy.video_stack_reader.name]
+  depends_on                       = [vault_auth_backend.kubernetes, vault_kubernetes_auth_backend_config.backend_config]
 }
