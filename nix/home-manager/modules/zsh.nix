03b2796278e63aefa161495c4766c90cfe013252
Aidan Hilt <aidanhilt2@gmail.com>
2024-07-21 22:52:04 -0400

    Trying something else

--- nix/home-manager/modules/zsh.nix
+++ nix/home-manager/modules/zsh.nix
@@ -13,13 +13,16 @@ in
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    initExtra = builtins.readfile(p10k-config);
-
     plugins = [
       {
         name = "powerlevel10k";
         src = pkgs.zsh-powerlevel10k;
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = p10k-config;
+        file = ".p10k.zsh"
       }
     ];
 
