bf6c79c1496e7d9d640683ac62bee4f5f43f6722
Aidan Hilt <aidanhilt2@gmail.com>
2024-07-21 21:54:42 -0400

    Makes sense

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -22,12 +22,6 @@ let
   # environment.
   home.packages = [];
 
-  home.file = {
-    ".p10k.zsh" = {
-      source = ./.p10k.zsh;
-    };
-  };
-
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
