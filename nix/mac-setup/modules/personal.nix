885e93a61853c0d10f4bc80c409aea0478e5ed07
Aidan Hilt <aidanhilt2@gmail.com>
2024-10-15 23:09:07 -0400

    This better work

--- nix/mac-setup/modules/personal.nix
+++ nix/mac-setup/modules/personal.nix
@@ -2,14 +2,31 @@
 
 let
   p2n = (inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; });
+
+  pypkgs-build-requirements = {
+    argparse = ["setuptools"];
+    click = ["setuptools"];
+    asyncio = ["setuptools"];
+    shutils = ["setuptools"];
+  };
+
+  p2n-overrides = p2n.defaultPoetryOverrides.extend (self: super:
+    builtins.mapAttrs (package: build-requirements:
+      (builtins.getAttr package super).overridePythonAttrs (old: {
+        buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg: if builtins.isString pkg then builtins.getAttr pkg super else pkg) build-requirements);
+      })
+    ) pypkgs-build-requirements
+  );
+
   atils = p2n.mkPoetryApplication {
     projectDir = builtins.fetchGit {
       url = "https://github.com/AidanHilt/PersonalMonorepo.git";
       ref = "feat/nix-darwin";
-      rev = "c5ef5b16ddc542d13b6ff4aa524b63d88bcaaa28"; #pragma: allowlist secret
+      rev = "6d6f65206bf1adbc99993081861b88525305da68"; #pragma: allowlist secret
     } + "/atils";
 
-    overrides = p2n.overrides.withoutDefaults (final: prev: { ruff = pkgs.ruff; });
+    overrides = p2n-overrides;
+    preferWheels = true;
   };
 in
 
