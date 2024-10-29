801752283c1ecf8125ebf83518ab73efecca0a27
Aidan Hilt <11202897+AidanHilt@users.noreply.github.com>
2024-10-28 23:04:46 -0400

    Let's take it from the top (#104)
    * Let's take it from the top
    
    * If we're gonna take it from the top, we gotta take it from the top

--- nix/mac-setup/nix-setup.sh
+++ nix/mac-setup/nix-setup.sh
@@ -4,51 +4,46 @@ curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix 
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-source /etc/zshrc
+/nix/var/nix/profiles/default/bin/nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
 
-/run/current-system/sw/bin/nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+/nix/var/nix/profiles/default/bin/nix-channel --add http://nixos.org/channels/nixpkgs-unstable nixpkgs
+/nix/var/nix/profiles/default/bin/nix-channel --update
 
 ./result/bin/darwin-installer
 
 mkdir -p ~/.config/nix-darwin
-# mkdir -p ~/.config/nixpkgs
-
-# config_content='
-# {
-#   packageOverrides = pkgs: {
-#     nur = import (builtins.fetchTarball {
-#       url = "https://github.com/nix-community/NUR/archive/master.tar.gz";
-#     }) {
-#       inherit pkgs;
-#     };
-#   };
-# }
-# '
-
-# # Check if config.nix already exists
-# if [[ -f ~/.config/nixpkgs/config.nix ]]; then
-#   # Check if the packageOverrides are already present
-#   if grep -q "packageOverrides" ~/.config/nixpkgs/config.nix; then
-#     echo "packageOverrides already exists in ~/.config/nixpkgs/config.nix"
-#     echo "Please manually update the file if you need to change the configuration."
-#   else
-#     # Append the new configuration to the existing file
-#     echo "$config_content" >> ~/.config/nixpkgs/config.nix
-#     echo "Updated ~/.config/nixpkgs/config.nix with the new packageOverrides configuration"
-#   fi
-# else
-#   # Create a new config.nix with the provided content
-#   echo "$config_content" > ~/.config/nixpkgs/config.nix
-#   echo "Created ~/.config/nixpkgs/config.nix with the packageOverrides configuration"
-# fi
-
-# TODO download nix config flake, set hostname, and build/apply
-# We're pretty much dependent on Git, so we need to get that installed in order to set up our whole system
-xcode-select --install
 
 git clone https://github.com/AidanHilt/PersonalMonorepo.git
 
-echo "darwin-rebuild --flake ~/PersonalMonorepo/nix/mac-setup" | pbcopy
+hostnames=("virtual-machine")
+
+# Print available options
+echo "\nAvailable hostnames:"
+i=1
+for hostname in "$hostnames[@]"; do
+  echo "${i}) ${hostname}"
+  i=$((i+1))
+done
+
+# Get user selection with input validation
+while true; do
+  echo -n "\nSelect hostname (1-${#hostnames[@]}): "
+  read selection
+  index=$((selection-1))
+
+  # Check if input is a number and within valid range
+  if [[ "$selection" =~ ^[0-9]+$ ]] && [ "$selection" -ge 1 ] && [ "$selection" -le ${#hostnames[@]} ]; then
+    selected_hostname=${hostnames[@]:0:1}
+    echo "\nYou selected: $selected_hostname"
+    break
+  else
+    echo "Invalid selection. Please enter a number between 1 and ${#hostnames[@]}"
+  fi
+done
+
+sudo mv /etc/auto_master /etc/auto_master.before-nix-darwin
+
+echo "darwin-rebuild switch --flake ~/PersonalMonorepo/nix/mac-setup#$selected_hostname" | pbcopy
 
 echo "This script has finished running. Please open a new terminal, and then CMD+V to run the nix command that will run the initial nix setup"
 echo "Once the nix installation has been completed, you can open a new terminal and run 'finish-setup' to finalize the setup"
