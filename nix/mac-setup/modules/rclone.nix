1c5c752ff225b888641f9b299a6340b66883f518
aidan <aidanhilt2@gmail.com>
2024-11-07 10:53:41 -0500

    Hold up

--- nix/mac-setup/modules/rclone.nix
+++ nix/mac-setup/modules/rclone.nix
@@ -1,9 +1,21 @@
 { inputs, pkgs, globals, ... }:
 
+let
+  sync-wallpapers = pkgs.writeShellScriptBin "sync-wallpapers" ''
+  rclone bisync drive:Wallpapers ~/Wallpapers --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync
+  '';
+
+  sync-keepass = pkgs.writeShellScriptBin "sync-keepass" ''
+  rclone bisync drive:KeePass ~/KeePass --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync
+  '';
+in
+
 {
 
   environment.systemPackages = [
     pkgs.rclone
+    sync-wallpapers
+    sync-keepass
   ];
 
   # TODO update this so we encrypt files in Google Drive, for extra oomph
