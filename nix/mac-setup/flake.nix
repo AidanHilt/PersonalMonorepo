da3a6c9dc0b8576627216c5effc850fd6a470f32
aidan <aidanhilt2@gmail.com>
2024-11-07 22:55:50 -0500

    Nix commit

--- nix/mac-setup/flake.nix
+++ nix/mac-setup/flake.nix
@@ -26,8 +26,8 @@
       username = "aidan";
       personalConfig = builtins.fetchGit {
         url = "https://github.com/AidanHilt/PersonalMonorepo.git";
-        ref = "feat/password-manager-browser-extension";
-        rev = "af43e3b8a4b57172f6e43bf4424e639b08f85363"; #pragma: allowlist secret
+        ref = "master";
+        rev = "fde068fe77abf2a23cc7c4f8a91cd1ecc3e8dfef"; #pragma: allowlist secret
       } + "/nix";
     };
 
--- nix/mac-setup/modules/common.nix
+++ nix/mac-setup/modules/common.nix
@@ -11,6 +11,7 @@ let
   cd ~/PersonalMonorepo
   git add nix/*
   git commit -m "Nix commit"
+  git push
 '';
 
   argocd-commit = pkgs.writeShellScriptBin "argocd-commit" ''
