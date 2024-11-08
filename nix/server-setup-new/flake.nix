8c8e10ff485cb586ee63bd20a2e5159f6e42aa69
aidan <aidanhilt2@gmail.com>
2024-11-07 23:04:44 -0500

    Nix commit

--- nix/server-setup-new/flake.nix
+++ nix/server-setup-new/flake.nix
@@ -10,7 +10,7 @@
     agenix.url = "github:ryantm/agenix";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
 
-    PersonalMonorepo = {
+    personalMonorepo = {
       url = "github:aidanhilt/PersonalMonorepo";
       flake = false;
     };
