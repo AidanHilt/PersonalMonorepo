862c7124ae5b0c39ee796443f4df8454444406a4
aidan <aidanhilt2@gmail.com>
2024-11-08 08:50:09 -0500

    Nix commit

--- nix/server-setup-new/modules/common.nix
+++ nix/server-setup-new/modules/common.nix
@@ -10,10 +10,9 @@
     isNormalUser = true;
 
     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIImw5CsGmsR1WTunv5bvNcozmoUSgJf76RMvy6SZtA2R aidan@hyperion"];
-
-    programs.zsh.enable = true;
   };
 
+  programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
   environment.pathsToLink = [ "/share/zsh" ];
 
