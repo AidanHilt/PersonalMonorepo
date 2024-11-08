e0abfaf14b0cf73f241b6ac1fd8e3ab778f91472
aidan <aidanhilt2@gmail.com>
2024-11-08 09:27:59 -0500

    Nix commit

--- nix/home-manager/machine-configs/home-server.nix
+++ nix/home-manager/machine-configs/home-server.nix
@@ -14,7 +14,7 @@ in
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
 
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
