311748b02d5edf86584ddda8ff06b749845eed5c
Aidan Hilt <ahilt@uchicago.edu>
2024-11-11 17:13:34 -0500

    Nix commit

--- nix/mac-setup/modules/work-docker.nix
+++ nix/mac-setup/modules/work-docker.nix
@@ -28,7 +28,7 @@ let
   '';
 
   gen3-helm-install = pkgs.writeShellScriptBin "gen3-helm-install" ''
-  cd ~/Gen3Repos/gen3-helm/helm/gen3 && helm dependency update && helm depdency build
+  cd ~/Gen3Repos/gen3-helm/helm/gen3 && helm dependency update && helm depdendency build
   helm upgrade --install gen3 ~/Gen3Repos/gen3-helm/helm/gen3
   '';
 in
