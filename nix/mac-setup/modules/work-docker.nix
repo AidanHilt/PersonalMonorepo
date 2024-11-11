0802f3d798b6d30430f80c7e5be03350a6ea5f83
Aidan Hilt <ahilt@uchicago.edu>
2024-11-11 16:53:30 -0500

    Nix commit

--- nix/mac-setup/modules/work-docker.nix
+++ nix/mac-setup/modules/work-docker.nix
@@ -1,7 +1,27 @@
 { inputs, pkgs, globals, ... }:
 
 let
+  gen3-cluster-setup = pkgs.writeShellScriptBin "gen3-cluster-setup" ''
+  cat <<EOF | kind create cluster --config=-
+  kind: Cluster
+  apiVersion: kind.x-k8s.io/v1alpha4
+  nodes:
+  - role: control-plane
+    kubeadmConfigPatches:  - |
+      kind: InitConfiguration
+      nodeRegistration:
+        kubeletExtraArgs:
+          node-labels: "ingress-ready=true"
+    extraPortMappings:
+    - containerPort: 80
+      hostPort: 80
+      protocol: TCP
+    - containerPort: 443
+      hostPort: 443
+      protocol: TCP
+  EOF
 
+  '';
 in
 
 {
@@ -9,6 +29,7 @@ in
     pkgs.colima
     pkgs.docker
     pkgs.docker-buildx
+    gen3-cluster-setup
   ];
 
   # Launch Colima on startup, so we always have docker working
@@ -23,9 +44,5 @@ in
       StandardOutPath = "/tmp/colima-autostart.log";
       StandardErrorPath = "/tmp/colima-autostart.error.log";
     };
-  };
-
-  environment.shellAliases = {
-    "docker build" = "docker-buildx build";
   };
 }
