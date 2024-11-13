6191ad815072ec54b218c5e068cc06c393abea43
Aidan <aidanhilt2@gmail.com>
2024-11-13 09:28:19 -0500

    First commit of our Windows work, now let's get Nix up

---
+++ nix/wsl-setup/machines/wsl-machine.nix
@@ -0,0 +1,37 @@
+{ inputs, globals, nixpkgs, ...}:
+
+with inputs;
+
+let
+  home-dot-nix = inputs.personalMonorepo + "/nix/home-manager/machine-configs/home-server.nix";
+
+  system = "aarch64-linux";
+  pkgs = import nixpkgs {
+    config.allowUnfree = true;
+    inherit system;
+  };
+in
+
+nixpkgs.lib.nixosSystem {
+  modules = [
+    home-manager.nixosModules.home-manager {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.backupFileExtension = "bak";
+      home-manager.extraSpecialArgs = { inherit inputs globals pkgs; };
+      home-manager.users.nixos = import home-dot-nix {inherit inputs globals pkgs; system = pkgs.system; lib = home-manager.lib; };
+    }
+
+    ({ inputs, globals, ...}: {
+      networking.hostName = "wsl-machine";
+      nixpkgs.hostPlatform = "x86_64-linux";
+
+      wsl = {
+        enable = true;
+        defaultUser = "nixos";
+      };
+    })
+
+    inputs.nixos-wsl.nixosModules.wsl
+  ];
+}
