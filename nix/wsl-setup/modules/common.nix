9b2a66916061d5d3eac8ec2601523f3882b72982
Aidan Hilt <aidanhilt2@gmail.com>
2024-11-15 03:01:21 +0000

    Nix commit

--- nix/wsl-setup/modules/common.nix
+++ nix/wsl-setup/modules/common.nix
@@ -25,4 +25,10 @@
  # environment.pathsToLink = [ "/share/zsh" ];
 
   services.openssh.enable = true;
+
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
 }
