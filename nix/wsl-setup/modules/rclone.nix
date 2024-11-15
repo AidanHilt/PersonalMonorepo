4fb2f43e205d956ade0e0c12895d3b436ddec301
Aidan Hilt <aidanhilt2@gmail.com>
2024-11-15 04:49:44 +0000

    Nix commit

--- nix/wsl-setup/modules/rclone.nix
+++ nix/wsl-setup/modules/rclone.nix
@@ -2,11 +2,11 @@
 
 let
   sync-wallpapers = pkgs.writeShellScriptBin "sync-wallpapers" ''
-  rclone bisync drive:Wallpapers ~/Wallpapers --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync
+  rclone bisync drive:Wallpapers $WINDOWS_HOME_DIR/Wallpapers --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync
   '';
 
   sync-keepass = pkgs.writeShellScriptBin "sync-keepass" ''
-  rclone bisync drive:KeePass ~/KeePass --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync
+  rclone bisync drive:KeePass $WINDOWS_HOME_DIR/KeePass --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync
   '';
 
   windows-home-dir = "/mnt/c/Users/Aidan";
