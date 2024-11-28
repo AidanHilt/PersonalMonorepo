c3ffdecee2c74bee7998abfbedef43e845071f52
aidan <aidanhilt2@gmail.com>
2024-11-28 00:51:17 -0500

    Nix commit

--- nix/mac-setup/modules/kubernetes.nix
+++ nix/mac-setup/modules/kubernetes.nix
@@ -7,7 +7,7 @@
   # TODO let's try and get this set up right, right now the permissions are fucked
   # Maybe we need a ticket?
   age.secrets.kubeconfig = {
-    file = ../secrets/kubeconfig.age;
+    file = globals.nixConfig + /secrets/kubeconfig.age;
     path = "/Users/${globals.username}/.kube/config";
     owner = "${globals.username}";
     mode = "700";
