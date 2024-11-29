f93860fe2cbd5ee713a79d9ac5196fdb3df36b8c
aidan <aidanhilt2@gmail.com>
2024-11-29 13:05:17 -0500

    Nix commit

--- nix/server-setup-new/modules/adguard.nix
+++ nix/server-setup-new/modules/adguard.nix
@@ -7,12 +7,19 @@
 
   services.adguardhome = {
     enable = true;
-    #mutableSettings = false;
+    mutableSettings = false;
     port = 3000;
   };
 
   networking.firewall = {
     allowedTCPPorts = [ 53 3000 ];
     allowedUDPPorts = [ 53 ];
+  };
+
+  age.secrets.rclone-config = {
+    file = globals.nixConfig + "/secrets/adguardhome.age";
+    path = "/var/lib/private/AdGuardHome/AdGuardHome.yaml";
+    owner = "aidan";
+    mode = "744";
   };
 }
