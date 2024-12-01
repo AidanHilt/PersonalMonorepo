39896ae31176f3ddba979191b4fd82e91d24f8f7
aidan <aidanhilt2@gmail.com>
2024-12-01 16:06:03 -0500

    Nix commit

--- nix/server-setup-new/modules/keepalived-staging.nix
+++ nix/server-setup-new/modules/keepalived-staging.nix
@@ -9,6 +9,7 @@
     enable = true;
 
     vrrpInstances.adguardhome = {
+      interface = "enp0s1";
       priority = 100;
       virtualRouterId = 81;
       virtualIps = [{ addr = "192.168.86.19"; }];
