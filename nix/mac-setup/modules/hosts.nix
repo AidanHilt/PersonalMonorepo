b0e25c01eec270c02a1d9fe10496060426767d72
aidan <aidanhilt2@gmail.com>
2024-12-01 00:30:37 -0500

    Nix commit

--- nix/mac-setup/modules/hosts.nix
+++ nix/mac-setup/modules/hosts.nix
@@ -1,12 +1,11 @@
-{ inputs, pkgs, globals, ... }:
+{ inputs, pkgs, globals, config, ... }:
 
 {
   age.secrets.rclone-config = {
     file = globals.nixConfig + "/secrets/hosts.age";
-    path = "/etc/hosts";
     owner = "root";
     mode = "644";
   };
 
-  networking.hostFiles = [];
+  networking.hostFiles = [ config.age.secrets.rclone-config.path ];
 }
