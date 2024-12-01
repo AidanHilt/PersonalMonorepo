d571750ce750608dc095604e72dbe23874b84cf4
aidan <aidanhilt2@gmail.com>
2024-12-01 00:38:58 -0500

    Nix commit

--- nix/mac-setup/machines/personal-macbook.nix
+++ nix/mac-setup/machines/personal-macbook.nix
@@ -29,7 +29,7 @@ darwin.lib.darwinSystem {
 
     ../modules/common.nix
     ../modules/personal.nix
-    #../modules/hosts.nix
+    ../modules/hosts.nix
 
   ];
   specialArgs = { inherit inputs globals; };
