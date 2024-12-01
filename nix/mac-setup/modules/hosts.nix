a500d306aac3af30935718695be9cdcbe621f1ed
aidan <aidanhilt2@gmail.com>
2024-12-01 00:48:02 -0500

    Nix commit

--- nix/mac-setup/modules/hosts.nix
+++ nix/mac-setup/modules/hosts.nix
@@ -3,9 +3,9 @@
 {
   age.secrets.hosts-file = {
     file = globals.nixConfig + "/secrets/hosts.age";
-    owner = "root";
-    mode = "644";
-    symlink = false;
+    # owner = "root";
+    # mode = "644";
+    # symlink = false;
   };
 
   # TODO when https://github.com/LnL7/nix-darwin/pull/939 or similar gets merged, don't just plop a hosts file
