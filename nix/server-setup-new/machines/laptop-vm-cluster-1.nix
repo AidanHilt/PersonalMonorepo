5552ece905a13f4ebd6c52ec1e97747f094e56ea
aidan <aidanhilt2@gmail.com>
2024-12-12 22:12:00 -0500

    Nix commit

--- nix/server-setup-new/machines/laptop-vm-cluster-1.nix
+++ nix/server-setup-new/machines/laptop-vm-cluster-1.nix
@@ -38,7 +38,7 @@ nixpkgs.lib.nixosSystem {
 
       age.secrets.rke-token = {
         file = globals.nixConfig + "/secrets/rke-token-mac-cluster.age";
-        path = "/var/lib/rancher/rke2/server/token";
+        path = "/var/lib/rancher/rke2/server/node-token";
       };
 
       boot.loader.systemd-boot.enable = true;
