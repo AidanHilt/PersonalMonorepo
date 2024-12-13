4ebed79b24d9300d215332fd9a10959bfba078f8
aidan <aidanhilt2@gmail.com>
2024-12-12 22:08:10 -0500

    Nix commit

--- nix/server-setup/modules/rke-secondary.nix
+++ nix/server-setup/modules/rke-secondary.nix
@@ -11,27 +11,14 @@ in
   };
 
   config = {
-    systemd.services.rke2 = {
-      description = "RKE2 Kubernetes Service";
-      after = [ "network-online.target" ];
-      wants = [ "network-online.target" ];
-      wantedBy = [ "multi-user.target" ];
+    services.rke2 = {
+      enable = true;
+      nodeName = "mac-cluster-server-2";
 
-      serviceConfig = {
-        ExecStart = "${pkgs.rke2}/bin/rke2 server --server ${cfg.serverAddr} --token-file /var/lib/rancher/rke2/server/token";
-        Restart = "always";
-        RestartSec = "5s";
-      };
-   };
-    networking.firewall = {
-     # See https://docs.rke2.io/install/requirements#inbound-network-rules for details
-     allowedTCPPorts = [ 6443 9345 ];
-     allowedTCPPortRanges = [
-       {
-         from = 30000;
-         to = 32767;
-       }
-     ];
-   };
- };
+      tokenFile = "/var/lib/rancher/rke2/server/token";
+      serverAddr = "https://192.168.86.192:9345";
+
+      cni = "calico";
+    };
+  }
 }
