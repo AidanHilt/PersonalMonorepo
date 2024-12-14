ba5df94afe71379f885eb3150140e005ec94ad42
aidan <aidanhilt2@gmail.com>
2024-12-13 23:17:16 -0500

    Nix commit

--- nix/server-setup-new/modules/rke-primary.nix
+++ nix/server-setup-new/modules/rke-primary.nix
@@ -14,7 +14,7 @@
 
   networking.firewall = {
     # See https://docs.rke2.io/install/requirements#inbound-network-rules for details
-    allowedTCPPorts = [ 6443 6444 9345 10250 2379 2380 2381 ];
+    allowedTCPPorts = [ 6443 6444 9345 10250 2379 2380 2381 9099 ];
     allowedTCPPortRanges = [
       {
         from = 30000;
