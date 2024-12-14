51bbad207235fe38b1b36f922ac06455733afad9
aidan <aidanhilt2@gmail.com>
2024-12-13 23:44:42 -0500

    Nix commit

--- nix/server-setup/modules/rke-secondary.nix
+++ nix/server-setup/modules/rke-secondary.nix
@@ -1,24 +1,24 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, pkgs, serverAddr, ... }:
 with lib;
-let
-  cfg = config.services.rke-secondary;
-in
+
 {
-  options.services.rke-secondary = {
-    serverAddr = mkOption {
-      type = types.str;
-    };
+  services.rke2 = {
+    enable = true;
+
+    tokenFile = "/var/lib/rancher/rke2/server/token";
+    serverAddr = "https://${serverAddr}:9345";
+
+    cni = "calico";
   };
 
-  config = {
-    services.rke2 = {
-      enable = true;
-      nodeName = "mac-cluster-server-2";
-
-      tokenFile = "/var/lib/rancher/rke2/server/token";
-      serverAddr = "https://192.168.86.192:9345";
-
-      cni = "calico";
-    };
-  }
+  networking.firewall = {
+    # See https://docs.rke2.io/install/requirements#inbound-network-rules for details
+    allowedTCPPorts = [ 6443 6444 9345 10250 2379 2380 2381 9099 5473 ];
+    allowedTCPPortRanges = [
+      {
+        from = 30000;
+        to = 32767;
+      }
+    ];
+  };
 }
