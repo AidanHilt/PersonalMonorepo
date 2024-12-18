aab5f07efa895e8c27f4ea28a1192b88fb9300b9
Aidan Hilt <ahilt@uchicago.edu>
2024-12-18 12:35:26 -0500

    Nix commit

--- nix/mac-setup/flake.nix
+++ nix/mac-setup/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/master";
 
-    nixpkgs-old-terraform.url = "github:nixos/nixpkgs/unstable?rev=5a8650469a9f8a1958ff9373bd27fb8e54c4365d";
+    nixpkgs-old-terraform.url = "github:nixos/nixpkgs?rev=5a8650469a9f8a1958ff9373bd27fb8e54c4365d";
 
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
