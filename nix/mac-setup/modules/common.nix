7a3418f4c7f799d2c7131873e6fb9fcf9dd7573c
aidan <aidanhilt2@gmail.com>
2024-12-24 10:08:58 -0500

    Nix commit

--- nix/mac-setup/modules/common.nix
+++ nix/mac-setup/modules/common.nix
@@ -27,6 +27,32 @@ let
   agenix -e kubeconfig.age
 '';
 
+  cluster-setup = pkgs.writeShellScriptBin "gen3-cluster-setup" ''
+  cat <<EOF | kind create cluster --config=-
+  kind: Cluster
+  apiVersion: kind.x-k8s.io/v1alpha4
+  nodes:
+  - role: control-plane
+    kubeadmConfigPatches:
+    - |
+      kind: InitConfiguration
+      nodeRegistration:
+        kubeletExtraArgs:
+          node-labels: "ingress-ready=true"
+    extraPortMappings:
+    - containerPort: 80
+      hostPort: 80
+      protocol: TCP
+    - containerPort: 443
+      hostPort: 443
+      protocol: TCP
+  EOF
+  '';
+
+  cluster-teardown = pkgs.writeShellScriptBin "gen3-cluster-teardown" ''
+  kind delete cluster
+  '';
+
 in
 
 {
