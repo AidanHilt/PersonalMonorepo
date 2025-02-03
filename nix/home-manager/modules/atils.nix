4fe9a12084fc32c249ca6eb843d099ae2e6f000b
aidan <aidanhilt2@gmail.com>
2025-02-02 22:14:31 -0500

    Nix commit

--- nix/home-manager/modules/atils.nix
+++ nix/home-manager/modules/atils.nix
@@ -35,7 +35,7 @@
     # ];
 
     sessionVariables = {
-      ATILS_INSTALL_DIR="/Users/${globals.username}/PersonalMonorepo";
+      ATILS_INSTALL_DIR="/Users/${globals.username}/PersonalMonorepo/atils";
       ATILS_KUBECONFIG_LOCATION="/Users/${globals.username}/.kube/";
       ATILS_SCRIPT_INSTALL_DIRECTORY="/Users/${globals.username}/PersonalMonorepo";
       ATILS_HELM_CHARTS_DIR="kubernetes/helm-charts";
