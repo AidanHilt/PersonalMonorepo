604a3995d67411309ad34b2388c67cd7b7210c08
aidan <aidanhilt2@gmail.com>
2025-03-14 23:11:25 -0400

    Nix commit

--- nix/mac-setup/flake.nix
+++ nix/mac-setup/flake.nix
@@ -19,7 +19,7 @@
     poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
 
     personalMonorepo = {
-      url = "github:aidanhilt/PersonalMonorepo/feat/external-user-1-retry";
+      url = "github:aidanhilt/PersonalMonorepo/feat/staging-cluster-setup";
       flake = false;
     };
   };
