462b0192a4a7f560c1f706ba686c56908dfff236
aidan <aidanhilt2@gmail.com>
2025-03-29 01:31:39 -0400

    Nix commit

--- nix/desktop-setup/flake.nix
+++ nix/desktop-setup/flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.11-nixos";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
