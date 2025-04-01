444423ec7821ac2acf27035dbb53c0e46a89ae2a
aidan <aidanhilt2@gmail.com>
2025-04-01 00:11:38 -0400

    Nix commit

--- nix/desktop-setup/machines/aarch64-linux/vm-desktop/configuration.nix
+++ nix/desktop-setup/machines/aarch64-linux/vm-desktop/configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
 
-      ../../modules/machine-categories/linux-desktop.nix
+      ../../../modules/machine-categories/linux-desktop.nix
     ];
 
   # Bootloader.
