b367dc0972f2c414d876545504320513bbb825ce
aidan <aidanhilt2@gmail.com>
2025-04-04 00:43:20 -0400

    Nix commit

--- nix/desktop-setup/flake.nix
+++ nix/desktop-setup/flake.nix
@@ -66,8 +66,9 @@
           systemHosts;
 
       aarch64LinuxHosts = getConfigsForSystem "aarch64-linux";
+      x86_64LinuxHosts = getConfigsForSystem "x86_64-linux";
 
-      mkSystemNixos = name: system:
+      mkSystem = name: system:
         let
           moduleType = if system == "aarch64-darwin" then "darwinModules" else "nixosModules";
         in
@@ -84,8 +85,9 @@
         };
       };
 
-      aarch64LinuxConfigs = builtins.foldl' (accumulator: name: accumulator // (mkSystemNixos name "aarch64-linux")) {} aarch64LinuxHosts;
+      aarch64LinuxConfigs = builtins.foldl' (accumulator: name: accumulator // (mkSystem name "aarch64-linux")) {} aarch64LinuxHosts;
+      x86_64LinuxConfigs = builtins.foldl' (accumulator: name: accumulator // {mkSystem name "x86_64-linux"}) {} x86_64LinuxHosts;
     in {
-      nixosConfigurations = aarch64LinuxConfigs;
+      nixosConfigurations = aarch64LinuxConfigs // x86_64LinuxConfigs;
   };
 }
