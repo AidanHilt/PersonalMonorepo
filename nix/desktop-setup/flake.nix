561e7c4e8cbc1eeb5dc15143eaddbb6192652c9b
aidan <aidanhilt2@gmail.com>
2025-04-06 12:49:13 -0400

    Nix commit

--- nix/desktop-setup/flake.nix
+++ nix/desktop-setup/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     systems.url = "github:nix-systems/default";
