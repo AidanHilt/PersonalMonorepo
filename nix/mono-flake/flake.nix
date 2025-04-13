fd4bf513941ffc518dd4adc561d88fba4fa7a2bb
aidan <aidanhilt2@gmail.com>
2025-04-12 20:43:45 -0400

    Nix commit

---
+++ nix/mono-flake/flake.nix
@@ -0,0 +1,100 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    systems.url = "github:nix-systems/default";
+
+    agenix.url = "github:ryantm/agenix";
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
+
+    nur.url = "github:nix-community/nur";
+
+    personalMonorepo = {
+      url = "github:aidanhilt/PersonalMonorepo/staging-cluster-k8s-work";
+      flake = false;
+    };
+
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    wsl.url = "github:nix-community/NixOS-WSL";
+    wsl.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      globals = {
+        nixConfig = inputs.personalMonorepo + "/nix";
+      };
+
+      pkgsFor = inputs.nixpkgs.lib.genAttrs (import inputs.systems) (
+        system:
+          import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+              overlays = [
+                inputs.nur.overlays.default
+              ];
+          }
+      );
+
+      isNixosConfig = dir: builtins.pathExists (dir + "/configuration.nix");
+
+      getConfigsForSystem = system:
+        let
+          # Directory path for this system
+          systemDir = ./machines/${system};
+
+          # Read directory contents
+          systemDirContents = builtins.readDir systemDir;
+
+          # Filter to only include directories
+          systemDirNames = builtins.attrNames (nixpkgs.lib.filterAttrs
+            (name: type: type == "directory")
+            systemDirContents);
+
+          # Filter to only include directories with configuration.nix
+          systemHosts = builtins.filter (name:
+            isNixosConfig (systemDir + "/${name}")
+          ) systemDirNames;
+        in
+          systemHosts;
+
+      aarch64LinuxHosts = getConfigsForSystem "aarch64-linux";
+      x86_64LinuxHosts = getConfigsForSystem "x86_64-linux";
+
+      mkSystem = name: system:
+        let
+          moduleType = if system == "aarch64-darwin" then "darwinModules" else "nixosModules";
+          user-base = if system == "aarch64-darwin" then "/Users" else "/home";
+        in
+        {
+        "${name}" = nixpkgs.lib.nixosSystem {
+          inherit system;
+
+          specialArgs = {
+            machine-config = (import ./machines/${system}/${name}/values.nix) // {user-base = user-base;};
+            pkgs = pkgsFor.aarch64-linux;
+            inherit inputs globals;
+          };
+
+          modules = [
+            inputs.home-manager."${moduleType}".home-manager
+            ./machines/${system}/${name}/configuration.nix
+            inputs.agenix."${moduleType}".default
+            inputs.wsl."${moduleType}".wsl
+          ];
+        };
+      };
+
+      aarch64LinuxConfigs = builtins.foldl' (accumulator: name: accumulator // (mkSystem name "aarch64-linux")) {} aarch64LinuxHosts;
+      x86_64LinuxConfigs = builtins.foldl' (accumulator: name: accumulator // (mkSystem name "x86_64-linux")) {} x86_64LinuxHosts;
+    in {
+      nixosConfigurations = aarch64LinuxConfigs // x86_64LinuxConfigs;
+  };
+}
---
+++ nix/mono-flake/modules/roles/universal/development-machine.nix
@@ -0,0 +1,83 @@
+{ inputs, globals, pkgs, machine-config, ...}:
+
+let
+  # Platform-specific logic or setup
+  platform-apps = if pkgs.system == "aarch64-darwin" then with pkgs; [ defaultbrowser ] else with pkgs; [];
+
+  rebuild-app = if pkgs.system == "aarch64-darwin" then "darwin-rebuild" else "nixos-rebuild";
+
+  # Shortcut scripts
+  argocd-commit = pkgs.writeShellScriptBin "argocd-commit" ''
+  cd $PERSONAL_MONOREPO_LOCATION
+  git add kubernetes/
+  git commit -m "Argocd commit"
+  git push
+'';
+
+  nix-commit = pkgs.writeShellScriptBin "nix-commit" ''
+  cd $PERSONAL_MONOREPO_LOCATION
+  git add nix/*
+  git commit -m "Nix commit"
+  git push
+'';
+
+  reset-docker = pkgs.writeShellScriptBin "reset-docker" ''
+  docker container prune --force
+  docker image prune -a --force
+  docker builder prune --force
+'';
+
+  update = pkgs.writeShellScriptBin "update" ''
+  cd $PERSONAL_MONOREPO_LOCATION
+  git pull -q
+  ${rebuild-app} switch --flake $PERSONAL_MONOREPO_LOCATION/nix/mono-flake
+'';
+in
+
+{
+  imports = [
+    ./_kubernetes-admin.nix
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.variables = {
+    PERSONAL_MONOREPO_LOCATION = "${machine-config.user-base}/${machine-config.username}/PersonalMonorepo";
+  };
+
+  environment.systemPackages = with pkgs; [
+    act
+    cargo
+    check-jsonschema
+    detect-secrets
+    eza
+    gettext
+    inetutils
+    jq
+    p7zip
+    pipx
+    postgresql
+    pre-commit
+    rustc
+    syncthing
+    terraform
+    terragrunt
+    yarn
+    yq
+
+    argocd-commit
+    nix-commit
+    reset-docker
+    update
+  ] ++ platform-apps;
+
+  system.userActivationScripts = {
+    getPersonalMonorepo = {
+      text = ''
+        if [ ! -d "$PERSONAL_MONOREPO_LOCATION" ]; then
+          git clone https://github.com/AidanHilt/PersonalMonorepo.git "$PERSONAL_MONOREPO_LOCATION"
+        fi
+      '';
+    };
+  };
+}
