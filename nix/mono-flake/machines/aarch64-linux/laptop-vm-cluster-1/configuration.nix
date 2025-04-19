da87245d33c293c849e7a28eb616193f52ed57de
aidan <aidanhilt2@gmail.com>
2025-04-18 22:59:06 -0400

    Nix commit

--- nix/mono-flake/machines/aarch64-linux/laptop-vm-cluster-1/configuration.nix
+++ nix/mono-flake/machines/aarch64-linux/laptop-vm-cluster-1/configuration.nix
@@ -2,11 +2,11 @@
 
 {
   home-manager = {
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.backupFileExtension = "bak";
-    home-manager.extraSpecialArgs = { inherit inputs globals pkgs; };
-    home-manager.users.${machine-config.username} = import ../../../home-manager/shared-configs/server.nix {inherit inputs globals pkgs; system = pkgs.system; lib = home-manager.lib; };
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "bak";
+    extraSpecialArgs = { inherit inputs globals pkgs; };
+    users.${machine-config.username} = import ../../../home-manager/shared-configs/server.nix {inherit inputs globals pkgs; system = pkgs.system; lib = home-manager.lib; };
   };
 
   networking.hostname = "laptop-vm-cluster-1";
