49b8b32e5152cb74337576934e7fb953f63236a5
aidan <aidanhilt2@gmail.com>
2025-04-18 23:00:33 -0400

    Nix commit

--- nix/mono-flake/modules/roles/nixos/_fixed-ip-machine.nix
+++ nix/mono-flake/modules/roles/nixos/_fixed-ip-machine.nix
@@ -2,7 +2,7 @@
 
 # TODO Let's add a with statement here
 {
-  networking = mkIf ${machine-config.networking.fixedIp} {
+  networking = mkIf machine-config.networking.fixedIp {
     defaultGateway = ${machine-config.networking.gateway};
     nameservers = ${machine-config.networking.nameservers};
     interfaces.enp0s1.ipv4.addresses = [
