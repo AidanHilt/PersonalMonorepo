891339efd8663da06ef706b5d3571329e669a8a6
aidan <aidanhilt2@gmail.com>
2025-04-18 23:11:07 -0400

    Nix commit

--- nix/mono-flake/modules/roles/nixos/_fixed-ip-machine.nix
+++ nix/mono-flake/modules/roles/nixos/_fixed-ip-machine.nix
@@ -3,7 +3,7 @@
 # TODO Let's add a with statement here
 {
   networking = pkgs.lib.mkIf machine-config.networking.fixedIp {
-    defaultGateway = machine-config.networking.gateway;
+    defaultGateway = machine-config.networking.defaultGateway;
     nameservers = machine-config.networking.nameservers;
     interfaces.enp0s1.ipv4.addresses = [
       {
