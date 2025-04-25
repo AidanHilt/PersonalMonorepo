2f0b6b499fdef47aa73769a8c0179f1e292c1311
aidan <aidanhilt2@gmail.com>
2025-04-24 21:53:43 -0400

    Nix commit

--- nix/mono-flake/machines/aarch64-linux/utf-machine/configuration.nix
+++ nix/mono-flake/machines/aarch64-linux/utf-machine/configuration.nix
@@ -14,4 +14,16 @@
   #   users.${machine-config.username} = import ./home.nix {inherit inputs globals pkgs machine-config; system = pkgs.system; lib = home-manager.lib; };
   # };
 
+  fileSystems = {
+  "/" = {
+    device = "/dev/disk/by-label/ROOTDIR";
+    fsType = "ext4";
+  };
+  "/boot" = {
+    device = "/dev/disk/by-label/BOOT";
+    fsType = "vfat";
+    options = [ "fmask=0022" "dmask=0022" ];
+  };
+};
+
 }
