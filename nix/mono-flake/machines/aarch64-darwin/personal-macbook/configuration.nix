94e702b31d421f838c35ff740f24d4263f51b7e6
aidan <aidanhilt2@gmail.com>
2025-04-30 09:23:43 -0400

    Nix commit

--- nix/mono-flake/machines/aarch64-darwin/personal-macbook/configuration.nix
+++ nix/mono-flake/machines/aarch64-darwin/personal-macbook/configuration.nix
@@ -2,11 +2,11 @@
 
 {
   home-manager = {
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.backupFileExtension = "bak";
-    home-manager.extraSpecialArgs = { inherit inputs globals pkgs; };
-    home-manager.users.aidan = import ./home.nix {inherit inputs globals pkgs; system = pkgs.system; lib = home-manager.lib; };
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "bak";
+    extraSpecialArgs = { inherit inputs globals pkgs; };
+    users.aidan = import ./home.nix {inherit inputs globals pkgs; system = pkgs.system; lib = home-manager.lib; };
   };
 
   networking.hostName = "hyperion";
