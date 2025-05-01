fbe68dfca0bb42d7971e05dfc7c5ca4a9109f0f1
aidan <aidanhilt2@gmail.com>
2025-05-01 09:51:33 -0400

    Nix commit

--- nix/mono-flake/home-manager/modules/zsh.nix
+++ nix/mono-flake/home-manager/modules/zsh.nix
@@ -15,7 +15,7 @@
       }
       {
         name = "powerlevel10k-config";
-        src = globals.nixConfig;
+        src = ../../../home-manager/config-files/.p10k.zsh;
         file = "home-manager/config-files/.p10k.zsh";
       }
     ];
