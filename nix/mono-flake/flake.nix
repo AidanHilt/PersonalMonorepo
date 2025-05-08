8c77a7e7745b38f24c951a409e4c9f7ae5372948
aidan <aidanhilt2@gmail.com>
2025-05-07 22:17:26 -0400

    Nix commit

--- nix/mono-flake/flake.nix
+++ nix/mono-flake/flake.nix
@@ -150,7 +150,7 @@
               home-manager.useUserPackages = true;
               home-manager.backupFileExtension = "bak";
               home-manager.extraSpecialArgs = { inherit inputs globals; pkgs = pkgsFor.aarch64-linux; };
-              home-manager.users.nixos = import ./home-manager/shared-configs/server.nix {inherit inputs globals; pkgs = pkgsFor.aarch64-linux; system = pkgs.system; lib = home-manager.lib; };
+              home-manager.users.nixos = import ./home-manager/shared-configs/server.nix {inherit inputs globals; pkgs = pkgsFor.aarch64-linux; system = "aarch64-linux"; lib = home-manager.lib; };
             }
 
             (
