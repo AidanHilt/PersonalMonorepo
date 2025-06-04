daf53e149f3d98a5deed5ea7e2a1e83d61588270
aidan <aidanhilt2@gmail.com>
2025-06-03 22:01:20 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/nixos-installer.nix
+++ nix/mono-flake/modules/roles/universal/nixos-installer.nix
@@ -90,7 +90,7 @@ print_success "Found ''${#MACHINE_NAMES[@]} machine configuration(s)"
 echo
 print_info "Available machine configurations:"
 for i in {1..''${#MACHINE_NAMES[@]}}; do
-    echo "  $i) ''${MACHINE_NAMES[$i]}"
+    echo "  $i) ''${MACHINE_NAMES[$i-1]}"
 done
 
 # Get user selection
