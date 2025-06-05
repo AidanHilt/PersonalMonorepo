a80ec586c032cf17b8abd4a6dba4d51fff2c36ad
aidan <aidanhilt2@gmail.com>
2025-06-05 10:05:57 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/nixos-installer.nix
+++ nix/mono-flake/modules/roles/universal/nixos-installer.nix
@@ -199,7 +199,7 @@ fi
 print_info "Starting nixos-anywhere deployment..."
 echo
 
-if [[ -v $NIXOS_ANYWHERE_ARGS ]]; then
+if [[ $NIXOS_ANYWHERE_ARGS_PROVIDED ]]; then
   read -ra CMD_ARRAY <<< "$NIXOS_ANYWHERE_ARGS"
   nix run github:nix-community/nixos-anywhere -- --flake "$FLAKE_DIR#$SELECTED_MACHINE" --target-host "root@$ip_address" "''${CMD_ARRAY[*]}"
 else
