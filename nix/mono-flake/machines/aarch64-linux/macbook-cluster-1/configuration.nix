762366385d124df84d6c73519cde83687c0f2173
aidan <aidanhilt2@gmail.com>
2025-06-06 09:55:57 -0400

    Nix commit

---
+++ nix/mono-flake/machines/aarch64-linux/macbook-cluster-1/configuration.nix
@@ -0,0 +1,32 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, machine-config, inputs, globals, ... }:
+
+{
+  imports = [
+      ./hardware-configuration.nix
+
+      ../../../modules/disko-configs/vda-single-disk.nix
+      ../../../modules/machine-categories/homelab-node.nix
+    ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "bak";
+    extraSpecialArgs = { inherit inputs globals pkgs machine-config; };
+    users.${machine-config.username} = import ../../home-manager/shared-configs/server.nix {inherit inputs globals pkgs machine-config; system = pkgs.system; lib = inputs.home-manager.lib; };
+  };
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+
+  users.users."${machine-config.username}".hashedPassword = "$y$j9T$47Fj09DL3ycTvCft06SAE1$FIYj3k6p1wzVOrZI.aLp5s7IBblimqa1/k/ACv9hiC/";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+}
--- nix/mono-flake/modules/machine-categories/linux-desktop-terminal.nix
+++
@@ -1,9 +0,0 @@
-{ inputs, globals, pkgs, machine-config, ...}:
-
-{
-  imports = [
-    ../roles/nixos/linux-universal.nix
-
-    ../roles/universal/development-machine.nix
-  ];
-}
