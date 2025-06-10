3bf39efa982fa28ebd2dc19db63b1a787504d66d
aidan <aidanhilt2@gmail.com>
2025-06-09 21:54:47 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/nixos-installer.nix
+++ nix/mono-flake/modules/roles/universal/nixos-installer.nix
@@ -489,8 +489,6 @@ else
     echo "Step 4: Replacing 127.0.0.1 with SSH host IP: $REPLACEMENT_IP"
 fi
 
-echo "$IP_ADDRESS"
-
 KUBECONFIG_CONTENT=$(echo "$KUBECONFIG_CONTENT" | sed "s/127\.0\.0\.1/$REPLACEMENT_IP/g")
 
 # Step 5: Output edited YAML to file
