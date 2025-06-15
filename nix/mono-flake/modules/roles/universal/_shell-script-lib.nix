1c3d7441358dba4982d89b5efe13e2a17ff7b78a
aidan <aidanhilt2@gmail.com>
2025-06-14 22:49:22 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/_shell-script-lib.nix
+++ nix/mono-flake/modules/roles/universal/_shell-script-lib.nix
@@ -53,7 +53,7 @@ echo "$USERNAME"
 generate-homelab-node-files = pkgs.writeShellScriptBin "generate-homelab-node-files" ''
 root=$(mktemp -d)
 mkdir -p "$root/etc/ssh"
-ssh-keygen -t ed25519 -f "$root/etc/ssh/ssh_host_ed25519_key" -N "" > /dev/null 2>&1
+ssh-keygen -t ed25519 -C "noname" -f "$root/etc/ssh/ssh_host_ed25519_key" -N "" > /dev/null 2>&1
 mkdir -p "$root/etc/rancher/rke2"
 age -i ~/.ssh/id_ed25519 -d "$PERSONAL_MONOREPO_LOCATION/nix/mono-flake/secrets/rke-config-$1.age" > "$root/etc/rancher/rke2/config.yaml"
 echo $root | tr -d "\n"
