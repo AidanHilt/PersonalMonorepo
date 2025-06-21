7220ea3bba22647abc0d76ea539d10e0cf9e19e7
aidan <aidanhilt2@gmail.com>
2025-06-20 23:23:50 -0400

    Nix commit

--- nix/mono-flake/flake.nix
+++ nix/mono-flake/flake.nix
@@ -81,112 +81,64 @@
         baseOverlays
         platformOverlays;
 
-      isNixosConfig = dir: builtins.pathExists (dir + "/configuration.nix");
-
-      getConfigsForSystem = system:
-        let
-          # Directory path for this system
-          systemDir = ./machines/${system};
-
-          # Read directory contents
-          systemDirContents = builtins.readDir systemDir;
-
-          # Filter to only include directories
-          systemDirNames = builtins.attrNames (nixpkgs.lib.filterAttrs
-            (name: type: type == "directory")
-            systemDirContents);
-
-          # Filter to only include directories with configuration.nix
-          systemHosts = builtins.filter (name:
-            isNixosConfig (systemDir + "/${name}")
-          ) systemDirNames;
-        in
-          systemHosts;
-
-      aarch64DarwinHosts = getConfigsForSystem "aarch64-darwin";
-      aarch64LinuxHosts = getConfigsForSystem "aarch64-linux";
-      x86_64LinuxHosts = getConfigsForSystem "x86_64-linux";
-
-      mkSystem = name: system:
-        let
-          systemFunction = if system == "aarch64-darwin" then darwin.lib.darwinSystem else nixpkgs.lib.nixosSystem;
-          moduleType = if system == "aarch64-darwin" then "darwinModules" else "nixosModules";
-          user-base = if system == "aarch64-darwin" then "/Users" else "/home";
-
-          platformModules = if moduleType == "nixosModules" then [inputs.wsl.nixosModules.wsl inputs.disko.nixosModules.disko] else [];
-        in
-        {
-        "${name}" = systemFunction {
-          inherit system;
-
-          specialArgs = {
-            machine-config = (import ./machines/${system}/${name}/values.nix { pkgs = pkgsFor.${system}; }) // {user-base = user-base; hostname = name;};
-            pkgs = pkgsFor.${system};
-            inherit inputs globals;
-          };
-
-          modules = [
-            ./machines/${system}/${name}/configuration.nix
-
-            inputs.home-manager.${moduleType}.home-manager
-            inputs.agenix.${moduleType}.default
-          ] ++ platformModules;
-        };
+      allConfigs = internalLib.configs.buildAllConfigs {
+        inherit systems pkgsFor inputs globals;
+        machinesDir = ./machines;
+        overlays = baseOverlays;
+        inherit platformOverlays;
       };
 
-      aarch64DarwinConfigs = builtins.foldl' (accumulator: name: accumulator // (mkSystem name "aarch64-darwin")) {} aarch64DarwinHosts;
-      aarch64LinuxConfigs = builtins.foldl' (accumulator: name: accumulator // (mkSystem name "aarch64-linux")) {} aarch64LinuxHosts;
-      x86_64LinuxConfigs = builtins.foldl' (accumulator: name: accumulator // (mkSystem name "x86_64-linux")) {} x86_64LinuxHosts;
     in {
-      nixosConfigurations = aarch64LinuxConfigs // x86_64LinuxConfigs // {
-        # How we build our bootstrap iso images
-        iso_image_x86 = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
-            inputs.home-manager.nixosModules.home-manager
-            ./modules/nixos/bootstrap-image.nix
-          ];
-        };
+      #darwinConfigurations = aarch64DarwinConfigs;
+      nixosConfigurations = allConfigs.nixosConfigurations;
 
-        iso-image-aarch64 = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
+      #  = aarch64LinuxConfigs // x86_64LinuxConfigs // {
+      #   # How we build our bootstrap iso images
+      #   iso_image_x86 = nixpkgs.lib.nixosSystem {
+      #     system = "x86_64-linux";
+      #     modules = [
+      #       (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+      #       inputs.home-manager.nixosModules.home-manager
+      #       ./modules/nixos/bootstrap-image.nix
+      #     ];
+      #   };
 
-          specialArgs = {
-            pkgs = pkgsFor.aarch64-linux;
-            inherit inputs globals;
-          };
+      #   iso-image-aarch64 = nixpkgs.lib.nixosSystem {
+      #     system = "aarch64-linux";
 
-          modules = [
-            (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
-            ./modules/roles/nixos/bootstrap-image.nix
+      #     specialArgs = {
+      #       pkgs = pkgsFor.aarch64-linux;
+      #       inherit inputs globals;
+      #     };
 
-            inputs.home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.backupFileExtension = "bak";
-              home-manager.extraSpecialArgs = { inherit inputs globals; pkgs = pkgsFor.aarch64-linux; };
-              home-manager.users.root = import ./home-manager/shared-configs/server.nix {
-                inherit inputs globals;
-                pkgs = pkgsFor.aarch64-linux;
-                system = "aarch64-linux";
-                lib = inputs.home-manager.nixosModules.home-manager.lib;
-              };
-            }
+      #     modules = [
+      #       (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+      #       ./modules/roles/nixos/bootstrap-image.nix
 
-            (
-              {pkgs, ...}: {
-                isoImage = {
-                  makeEfiBootable = true;
-                  makeUsbBootable = true;
-                  squashfsCompression = "zstd -Xcompression-level 6"; #way faster build time
-                };
-              }
-            )
-          ];
-        };
-      };
+      #       inputs.home-manager.nixosModules.home-manager {
+      #         home-manager.useGlobalPkgs = true;
+      #         home-manager.useUserPackages = true;
+      #         home-manager.backupFileExtension = "bak";
+      #         home-manager.extraSpecialArgs = { inherit inputs globals; pkgs = pkgsFor.aarch64-linux; };
+      #         home-manager.users.root = import ./home-manager/shared-configs/server.nix {
+      #           inherit inputs globals;
+      #           pkgs = pkgsFor.aarch64-linux;
+      #           system = "aarch64-linux";
+      #           lib = inputs.home-manager.nixosModules.home-manager.lib;
+      #         };
+      #       }
 
-      darwinConfigurations = aarch64DarwinConfigs;
+      #       (
+      #         {pkgs, ...}: {
+      #           isoImage = {
+      #             makeEfiBootable = true;
+      #             makeUsbBootable = true;
+      #             squashfsCompression = "zstd -Xcompression-level 6"; #way faster build time
+      #           };
+      #         }
+      #       )
+      #     ];
+      #   };
+      # };
   };
 }
