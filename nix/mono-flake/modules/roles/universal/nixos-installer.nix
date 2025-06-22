263d732e58c5b4254f6abddaf48c60e9c6ab93bc
aidan <aidanhilt2@gmail.com>
2025-06-22 16:01:17 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/nixos-installer.nix
+++ nix/mono-flake/modules/roles/universal/nixos-installer.nix
@@ -5,6 +5,7 @@
     ../../scripts/nixos-key-retrieval.nix
     ../../scripts/nixos-kubeconfig-retrieval.nix
     ../../scripts/nixos-remote-install.nix
+    ../../scripts/nixos-hardware-config-retrieval.nix
 
     ../../scripts/mono-flake-new-machine.nix
     ../../scripts/mono-flake-new-script.nix
