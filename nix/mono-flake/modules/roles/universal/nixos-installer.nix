bd195b8fa6d90b9b598e15176655b88e1a4cb510
aidan <aidanhilt2@gmail.com>
2025-06-22 22:58:43 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/nixos-installer.nix
+++ nix/mono-flake/modules/roles/universal/nixos-installer.nix
@@ -9,6 +9,6 @@
 
     ../../scripts/mono-flake-new-machine.nix
     ../../scripts/mono-flake-new-script.nix
-    ../../scripts/mono-flake-new-script.nix
+    ../../scripts/mono-flake-copy-machine.nix
   ];
 }
