3520e7beb39b841c87d7285e2c7a112a7037ff5a
aidan <aidanhilt2@gmail.com>
2025-06-23 23:31:23 -0400

    Nix commit

--- nix/mono-flake/machines/aarch64-darwin/hyperion/configuration.nix
+++ nix/mono-flake/machines/aarch64-darwin/hyperion/configuration.nix
@@ -5,8 +5,8 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     backupFileExtension = "bak";
-    extraSpecialArgs = { inherit inputs globals pkgs; };
-    users.aidan = import ./home.nix {inherit inputs globals pkgs; system = pkgs.system; lib = inputs.home-manager.lib; };
+    extraSpecialArgs = { inherit inputs globals pkgs machine-config; };
+    users.aidan = import ./home.nix {inherit inputs globals machine-config pkgs; system = pkgs.system; lib = inputs.home-manager.lib; };
   };
 
   imports = [
