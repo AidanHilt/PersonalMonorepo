da6ee0d99fb895fd3e7132b7dfc5680c1ec2e13d
aidan <aidanhilt2@gmail.com>
2025-06-29 10:26:21 -0400

    Nix commit

--- nix/mono-flake/modules/scripts/nixos-remote-install.nix
+++ nix/mono-flake/modules/scripts/nixos-remote-install.nix
@@ -59,12 +59,16 @@ if [ -z "$USERNAME" ]; then
   USERNAME=$(grep -E '^\s*defaultValues\s*=\s*"[^"]*"' "$DEFAULT_VALUES_FILE" | sed 's/.*"\([^"]*\)".*/\1/' | head -n1)
 fi
 
+echo "Huh 3?"
+
+
+
 if [ -z "$USERNAME" ]; then
   echo "No username found"
   exit 1
 fi
 
-echo "$USERNAME"
+echo "username: $USERNAME"
 '';
 
 nixos-remote-install = pkgs.writeShellScriptBin "nixos-remote-install" ''
