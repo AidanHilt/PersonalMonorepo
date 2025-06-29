4db3be497bb60aac93844eb4d934150e2a58d877
aidan <aidanhilt2@gmail.com>
2025-06-29 01:50:58 -0400

    Nix commit

--- nix/mono-flake/secrets/secrets.nix
+++ nix/mono-flake/secrets/secrets.nix
@@ -16,7 +16,7 @@ let
   # ===========================
 
   # A small test cluster we run on NixOS machines running as VMs on our laptop
-  laptop-cluster-1-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINZXQlJomzogfqA2/EVZR1cf15HHooW+2RKzwfPK7Wmd noname";
+  laptop-cluster-1-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBlX950UxPr/DUJsDUhH19fPJ8WYoA9GHEm0FgkFsqpN noname";
 
   laptop-cluster-machines = [laptop-cluster-1-system];
 
