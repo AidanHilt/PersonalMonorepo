8051106deac1e0bc9f79a48b212ff5cb5d94cf20
aidan <aidanhilt2@gmail.com>
2025-06-29 10:32:11 -0400

    Nix commit

--- nix/mono-flake/modules/scripts/nixos-remote-install.nix
+++ nix/mono-flake/modules/scripts/nixos-remote-install.nix
@@ -59,7 +59,7 @@ if [ -z "$USERNAME" ]; then
   USERNAME=$(grep -E '^\s*defaultValues\s*=\s*"[^"]*"' "$DEFAULT_VALUES_FILE" | sed 's/.*"\([^"]*\)".*/\1/' | head -n1)
 fi
 
-echo $USERNAME
+echo "$USERNAME"
 echo "Huh 3?"
 
 if [ -z "$USERNAME" ]; then
