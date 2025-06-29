f6c66abe5dbf9aff1deb760d10c95ce9fb0fc794
aidan <aidanhilt2@gmail.com>
2025-06-29 02:07:43 -0400

    Nix commit

--- nix/mono-flake/machines/aarch64-linux/laptop-cluster-1/configuration.nix
+++ nix/mono-flake/machines/aarch64-linux/laptop-cluster-1/configuration.nix
@@ -26,4 +26,6 @@
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
+
+  users.users."${machine-config.username}".hashedPassword = "$y$j9T$47Fj09DL3ycTvCft06SAE1$FIYj3k6p1wzVOrZI.aLp5s7IBblimqa1/k/ACv9hiC/";
 }
