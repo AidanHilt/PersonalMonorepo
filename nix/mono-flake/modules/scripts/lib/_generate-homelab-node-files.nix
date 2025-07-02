01263344ed93d15c44beaf59ee4550423882b11c
aidan <aidanhilt2@gmail.com>
2025-07-01 21:44:33 -0400

    Nix commit

---
+++ nix/mono-flake/modules/scripts/lib/_generate-homelab-node-files.nix
@@ -0,0 +1,22 @@
+{ inputs, globals, pkgs, machine-config, ...}:
+
+let
+generate-homelab-node-files = pkgs.writeShellScriptBin "generate-homelab-node-files" ''
+#!/bin/bash
+
+set -euo pipefail
+
+root=$(mktemp -d)
+mkdir -p "$root/etc/ssh"
+ssh-keygen -t ed25519 -C "noname" -f "$root/etc/ssh/ssh_host_ed25519_key" -N "" > /dev/null 2>&1
+mkdir -p "$root/etc/rancher/rke2"
+age -i ~/.ssh/id_ed25519 -d "$PERSONAL_MONOREPO_LOCATION/nix/mono-flake/secrets/rke-config-$1.age" > "$root/etc/rancher/rke2/config.yaml"
+echo $root | tr -d "\n"
+'';
+in
+
+{
+  environment.systemPackages = [
+    generate-homelab-node-files
+  ];
+}
--- nix/mono-flake/modules/scripts/nixos-kubeconfig-retrieval.nix
+++ nix/mono-flake/modules/scripts/nixos-kubeconfig-retrieval.nix
@@ -2,15 +2,6 @@
 
 let
 
-generate-homelab-node-files = pkgs.writeShellScriptBin "generate-homelab-node-files" ''
-root=$(mktemp -d)
-mkdir -p "$root/etc/ssh"
-ssh-keygen -t ed25519 -C "noname" -f "$root/etc/ssh/ssh_host_ed25519_key" -N "" > /dev/null 2>&1
-mkdir -p "$root/etc/rancher/rke2"
-age -i ~/.ssh/id_ed25519 -d "$PERSONAL_MONOREPO_LOCATION/nix/mono-flake/secrets/rke-config-$1.age" > "$root/etc/rancher/rke2/config.yaml"
-echo $root | tr -d "\n"
-'';
-
 nixos-kubeconfig-retrieval = pkgs.writeShellScriptBin "nixos-kubeconfig-retrieval" ''
 #!/bin/bash
 
@@ -153,8 +144,10 @@ in
 
 {
   imports = [
-  # TODO address this, we shouldn't be importing files that start with _.
-  ../roles/universal/_kubernetes-admin.nix
+    # TODO address this, we shouldn't be importing files that start with _.
+    ../roles/universal/_kubernetes-admin.nix
+
+    ./lib/default.nix
   ];
 
   environment.systemPackages = with pkgs; [
