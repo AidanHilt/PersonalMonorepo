fb833a5379d6d42a56536e759a308dc4552461f8
ahilt <aidanhilt2@gmail.com>
2025-07-04 13:38:50 -0400

    Nix commit

--- nix/mono-flake/home-manager/shared-configs/desktop-terminal.nix
+++ nix/mono-flake/home-manager/shared-configs/desktop-terminal.nix
@@ -15,11 +15,6 @@
     '';
   };
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [];
