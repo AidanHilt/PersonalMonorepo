bd5a0ac639aa204a8d6004c25341a62e39e26083
aidan <aidanhilt2@gmail.com>
2025-07-06 21:59:58 -0400

    Nix commit

--- nix/mono-flake/home-manager/modules/vscode.nix
+++ nix/mono-flake/home-manager/modules/vscode.nix
@@ -9,39 +9,41 @@ let
 in
 
 {
-  programs.vscode.profile.default = {
+  programs.vscode = {
     enable = true;
-
-    userSettings = builtins.fromJSON (builtins.readFile vscode-settings);
-
     mutableExtensionsDir = false;
-    extensions = with pkgs.vscode-marketplace; [
-      # These guys need to have the full title because of string stuff
-      pkgs.vscode-marketplace."4ops".terraform
 
-      bbenoist.nix
-      brunnerh.file-properties-viewer
-      charliermarsh.ruff
-      github.vscode-github-actions
-      github.vscode-pull-request-github
-      golang.go
-      gruntfuggly.todo-tree
-      hashicorp.hcl
-      mads-hartmann.bash-ide-vscode
-      mechatroner.rainbow-csv
-      ms-azuretools.vscode-docker
-      ms-kubernetes-tools.vscode-kubernetes-tools
-      ms-python.isort
-      ms-vscode-remote.remote-ssh
-      ms-vscode-remote.remote-ssh-edit
-      ms-vscode.powershell
-      ms-vscode.remote-explorer
-      ms-vsliveshare.vsliveshare
-      redhat.vscode-yaml
-      shd101wyy.markdown-preview-enhanced
-      slevesque.vscode-zipexplorer
-      timonwong.shellcheck
-      wholroyd.jinja
+    profiles.default = {
+      userSettings = builtins.fromJSON (builtins.readFile vscode-settings);
+
+      extensions = with pkgs.vscode-marketplace; [
+        # These guys need to have the full title because of string stuff
+        pkgs.vscode-marketplace."4ops".terraform
+
+        bbenoist.nix
+        brunnerh.file-properties-viewer
+        charliermarsh.ruff
+        github.vscode-github-actions
+        github.vscode-pull-request-github
+        golang.go
+        gruntfuggly.todo-tree
+        hashicorp.hcl
+        mads-hartmann.bash-ide-vscode
+        mechatroner.rainbow-csv
+        ms-azuretools.vscode-docker
+        ms-kubernetes-tools.vscode-kubernetes-tools
+        ms-python.isort
+        ms-vscode-remote.remote-ssh
+        ms-vscode-remote.remote-ssh-edit
+        ms-vscode.powershell
+        ms-vscode.remote-explorer
+        ms-vsliveshare.vsliveshare
+        redhat.vscode-yaml
+        shd101wyy.markdown-preview-enhanced
+        slevesque.vscode-zipexplorer
+        timonwong.shellcheck
+        wholroyd.jinja
      ];
+    };
   };
 }
