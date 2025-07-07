edf22d98ae193fb2fa58ffc496f1e9ce3e1e8570
aidan <aidanhilt2@gmail.com>
2025-07-06 22:09:18 -0400

    Nix commit

--- nix/mono-flake/machines/aarch64-darwin/hyperion/home.nix
+++ nix/mono-flake/machines/aarch64-darwin/hyperion/home.nix
@@ -10,7 +10,7 @@
     ../../../home-manager/modules/zsh.nix
   ];
 
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "25.05";
 
   programs.home-manager.enable = true;
 }
