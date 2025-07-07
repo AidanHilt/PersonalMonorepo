c5313204c3fb7c819915b1cc55f723bf2d8058da
aidan <aidanhilt2@gmail.com>
2025-07-06 21:32:11 -0400

    Nix commit

---
+++ nix/mono-flake/machines/x86_64-linux/big-boi-desktop/disko.nix
@@ -0,0 +1,39 @@
+{ inputs, globals, pkgs, system, machine-config, ...}:
+
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLB512HBJQ-000H1_S4GLNX0NC82463";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
