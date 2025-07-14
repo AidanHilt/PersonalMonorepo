31a219d9343bbcfa3b3e0e4c8629f567ef7444eb
ahilt <aidanhilt2@gmail.com>
2025-07-13 22:24:31 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/rclone.nix
+++ nix/mono-flake/modules/roles/universal/rclone.nix
@@ -81,142 +81,142 @@ in
     symlink = false;
   };
 
-  launchd.agents = pkgs.lib.mkIf (pkgs.lib.hasSuffix "darwin") {
-    rcloneKeepass = {
-      serviceConfig = {
-        UserName = "${machine-config.username}";
-        Program = /run/current-system/sw/bin/rclone;
-        ProgramArguments = [
-          "/run/current-system/sw/bin/rclone"
-          "bisync"
-          "drive:KeePass"
-          "/Users/${machine-config.username}/KeePass"
-          "--drive-skip-gdocs"
-          "--resilient"
-          "--create-empty-src-dirs"
-          "--fix-case"
-          "--slow-hash-sync-only"
-          "--config=/Users/${machine-config.username}/.config/rclone/rclone.conf"
-        ];
-        RunAtLoad = true;
-        StartInterval = 300;
-        StandardErrorPath = /tmp/rclone-keepass.log.err;
-        StandardOutPath = /tmp/rclone-keepass.log.out;
-      };
-    };
+  # launchd.agents = pkgs.lib.mkIf (pkgs.lib.hasSuffix "darwin") {
+  #   rcloneKeepass = {
+  #     serviceConfig = {
+  #       UserName = "${machine-config.username}";
+  #       Program = /run/current-system/sw/bin/rclone;
+  #       ProgramArguments = [
+  #         "/run/current-system/sw/bin/rclone"
+  #         "bisync"
+  #         "drive:KeePass"
+  #         "/Users/${machine-config.username}/KeePass"
+  #         "--drive-skip-gdocs"
+  #         "--resilient"
+  #         "--create-empty-src-dirs"
+  #         "--fix-case"
+  #         "--slow-hash-sync-only"
+  #         "--config=/Users/${machine-config.username}/.config/rclone/rclone.conf"
+  #       ];
+  #       RunAtLoad = true;
+  #       StartInterval = 300;
+  #       StandardErrorPath = /tmp/rclone-keepass.log.err;
+  #       StandardOutPath = /tmp/rclone-keepass.log.out;
+  #     };
+  #   };
 
 
-    rcloneWallpapers = {
-      serviceConfig = {
-        UserName = "${machine-config.username}";
-        Program = /run/current-system/sw/bin/rclone;
-        ProgramArguments = [
-          "/run/current-system/sw/bin/rclone"
-          "bisync"
-          "drive:Wallpapers"
-          "/Users/${machine-config.username}/Wallpapers"
-          "--drive-skip-gdocs"
-          "--resilient"
-          "--create-empty-src-dirs"
-          "--fix-case"
-          "--slow-hash-sync-only"
-          "--config=/Users/${machine-config.username}/.config/rclone/rclone.conf"
-        ];
-        RunAtLoad = true;
-        StartInterval = 300;
-        StandardErrorPath = /tmp/rclone-wallpapers.log.err;
-        StandardOutPath = /tmp/rclone-wallpapers.log.out;
-      };
-    };
-  };
+  #   rcloneWallpapers = {
+  #     serviceConfig = {
+  #       UserName = "${machine-config.username}";
+  #       Program = /run/current-system/sw/bin/rclone;
+  #       ProgramArguments = [
+  #         "/run/current-system/sw/bin/rclone"
+  #         "bisync"
+  #         "drive:Wallpapers"
+  #         "/Users/${machine-config.username}/Wallpapers"
+  #         "--drive-skip-gdocs"
+  #         "--resilient"
+  #         "--create-empty-src-dirs"
+  #         "--fix-case"
+  #         "--slow-hash-sync-only"
+  #         "--config=/Users/${machine-config.username}/.config/rclone/rclone.conf"
+  #       ];
+  #       RunAtLoad = true;
+  #       StartInterval = 300;
+  #       StandardErrorPath = /tmp/rclone-wallpapers.log.err;
+  #       StandardOutPath = /tmp/rclone-wallpapers.log.out;
+  #     };
+  #   };
+  # };
 
-  systemd = pkgs.lib.mkIf (! pkgs.lib.hasSuffix "darwin") {
-    timers = {
-      wallpaper-sync = {
-        wantedBy = [ "timers.target" ];
-        timerConfig = {
-          OnBootSec = "5m";
-          OnUnitActiveSec = "5m";
-          Unit = "wallpaper-sync.service";
-        };
-      };
+  # systemd = pkgs.lib.mkIf (! pkgs.lib.hasSuffix "darwin") {
+  #   timers = {
+  #     wallpaper-sync = {
+  #       wantedBy = [ "timers.target" ];
+  #       timerConfig = {
+  #         OnBootSec = "5m";
+  #         OnUnitActiveSec = "5m";
+  #         Unit = "wallpaper-sync.service";
+  #       };
+  #     };
 
-      keepass-sync = {
-        wantedBy = [ "timers.target" ];
-        timerConfig = {
-          OnBootSec = "5m";
-          OnUnitActiveSec = "5m";
-          Unit = "keepass-sync.service";
-        };
-      };
+  #     keepass-sync = {
+  #       wantedBy = [ "timers.target" ];
+  #       timerConfig = {
+  #         OnBootSec = "5m";
+  #         OnUnitActiveSec = "5m";
+  #         Unit = "keepass-sync.service";
+  #       };
+  #     };
 
-      documents-folder-sync = {
-        wantedBy = [ "timers.target" ];
-        timerConfig = {
-          OnBootSec = "30m";
-          OnUnitActiveSec = "30m";
-          Unit = "documents-folder-sync.service";
-        };
-      };
+  #     documents-folder-sync = {
+  #       wantedBy = [ "timers.target" ];
+  #       timerConfig = {
+  #         OnBootSec = "30m";
+  #         OnUnitActiveSec = "30m";
+  #         Unit = "documents-folder-sync.service";
+  #       };
+  #     };
 
-      lg-ghub-sync = {
-        wantedBy = [ "timers.target" ];
-        timerConfig = {
-          OnBootSec = "30m";
-          OnUnitActiveSec = "30m";
-          Unit = "lg-ghub-sync.service";
-        };
-      };
-    };
+  #     lg-ghub-sync = {
+  #       wantedBy = [ "timers.target" ];
+  #       timerConfig = {
+  #         OnBootSec = "30m";
+  #         OnUnitActiveSec = "30m";
+  #         Unit = "lg-ghub-sync.service";
+  #       };
+  #     };
+  #   };
 
-    services = {
-      wallpaper-sync = {
-        script = ''
-          set -xe
-          ${pkgs.rclone}/bin/rclone bisync drive:Wallpapers $WALLPAPER_DIR --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync --config /home/nixos/.config/rclone/rclone.conf
-        '';
+  #   services = {
+  #     wallpaper-sync = {
+  #       script = ''
+  #         set -xe
+  #         ${pkgs.rclone}/bin/rclone bisync drive:Wallpapers $WALLPAPER_DIR --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync --config /home/nixos/.config/rclone/rclone.conf
+  #       '';
 
-        serviceConfig = {
-          Type = "oneshot";
-          User = "root";
-        };
-      };
+  #       serviceConfig = {
+  #         Type = "oneshot";
+  #         User = "root";
+  #       };
+  #     };
 
-      keepass-sync = {
-        script = ''
-          set -xe
-          ${pkgs.rclone}/bin/rclone bisync drive:KeePass $KEEPASS_DIR --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync --config /home/nixos/.config/rclone/rclone.conf
-        '';
+  #     keepass-sync = {
+  #       script = ''
+  #         set -xe
+  #         ${pkgs.rclone}/bin/rclone bisync drive:KeePass $KEEPASS_DIR --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync --config /home/nixos/.config/rclone/rclone.conf
+  #       '';
 
-        serviceConfig = {
-          Type = "oneshot";
-          User = "root";
-        };
-      };
+  #       serviceConfig = {
+  #         Type = "oneshot";
+  #         User = "root";
+  #       };
+  #     };
 
-      documents-folder-sync = {
-        script = ''
-          set -xe
-          ${pkgs.rclone}/bin/rclone sync $WINDOWS_DOCUMENTS_DIR drive:Documents --drive-skip-gdocs --create-empty-src-dirs --fix-case --config /home/nixos/.config/rclone/rclone.conf
-        '';
+  #     documents-folder-sync = {
+  #       script = ''
+  #         set -xe
+  #         ${pkgs.rclone}/bin/rclone sync $WINDOWS_DOCUMENTS_DIR drive:Documents --drive-skip-gdocs --create-empty-src-dirs --fix-case --config /home/nixos/.config/rclone/rclone.conf
+  #       '';
 
-        serviceConfig = {
-          Type = "oneshot";
-          User = "root";
-        };
-      };
+  #       serviceConfig = {
+  #         Type = "oneshot";
+  #         User = "root";
+  #       };
+  #     };
 
-      lg-ghub-sync = {
-        script = ''
-          set -xe
-          ${pkgs.rclone}/bin/rclone sync $WINDOWS_GHUB_CONFIG_DIR drive:GHUB-Windows--drive-skip-gdocs --create-empty-src-dirs --fix-case --config /home/nixos/.config/rclone/rclone.conf
-        '';
+  #     lg-ghub-sync = {
+  #       script = ''
+  #         set -xe
+  #         ${pkgs.rclone}/bin/rclone sync $WINDOWS_GHUB_CONFIG_DIR drive:GHUB-Windows--drive-skip-gdocs --create-empty-src-dirs --fix-case --config /home/nixos/.config/rclone/rclone.conf
+  #       '';
 
-        serviceConfig = {
-          Type = "oneshot";
-          User = "root";
-        };
-      };
-    };
-  };
+  #       serviceConfig = {
+  #         Type = "oneshot";
+  #         User = "root";
+  #       };
+  #     };
+  #   };
+  # };
 }
