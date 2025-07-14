22847abcedf7adce3c0106da9e083215ea70e335
ahilt <aidanhilt2@gmail.com>
2025-07-14 15:26:03 -0400

    Nix commit

--- nix/mono-flake/home-manager/modules/keepassxc.nix
+++ nix/mono-flake/home-manager/modules/keepassxc.nix
@@ -10,19 +10,19 @@
     };
   };
 
-  # home.file."org.keepassxc.keepassxc_browser.json" = {
-  #   target = ".mozilla/native-messaging-hosts/org.keepassxc.keepassxc_browser.json";
-  #   text = ''
-  #     {
-  #       "allowed_extensions": [
-  #         "keepassxc-browser@keepassxc.org"
-  #       ],  
-  #       "description": "KeePassXC integration with native messaging support",
-  #       "name": "org.keepassxc.keepassxc_browser",
-  #       "path": "${pkgs.keepassxc}/bin/keepassxc-proxy",
-  #       "type": "stdio"
-  #     }
-  #   '';
-  # };
+  home.file."org.keepassxc.keepassxc_browser.json" = {
+    target = ".mozilla/native-messaging-hosts/org.keepassxc.keepassxc_browser.json";
+    text = ''
+      {
+        "allowed_extensions": [
+          "keepassxc-browser@keepassxc.org"
+        ],  
+        "description": "KeePassXC integration with native messaging support",
+        "name": "org.keepassxc.keepassxc_browser",
+        "path": "${pkgs.keepassxc}/bin/keepassxc-proxy",
+        "type": "stdio"
+      }
+    '';
+  };
 
 }
