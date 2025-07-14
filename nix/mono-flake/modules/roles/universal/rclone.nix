838395d052f38c310d78041b558f062385e2125c
ahilt <aidanhilt2@gmail.com>
2025-07-13 22:38:42 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/rclone.nix
+++ nix/mono-flake/modules/roles/universal/rclone.nix
@@ -65,10 +65,10 @@ in
   ] ++ wslScripts;
 
   environment.sessionVariables = {
-    WALLPAPER_DIR = "${wallpaperDir}";
-    KEEPASS_DIR = "${keePassDir}";
-    WINDOWS_DOCUMENTS_DIR = "${windowsDocumentsDir}";
-    WINDOWS_GHUB_CONFIG_DIR = "${windowsGHubConfigDir}";
+    WALLPAPER_DIR = wallpaperDir;
+    KEEPASS_DIR = keePassDir;
+    WINDOWS_DOCUMENTS_DIR = windowsDocumentsDir;
+    WINDOWS_GHUB_CONFIG_DIR = windowsGHubConfigDir;
   };
 
     # TODO update this so we encrypt files in Google Drive, for extra oomph
@@ -76,7 +76,7 @@ in
     file = ../../../secrets/rclone-config.age;
     path = "/${machine-config.userBase}/${machine-config.username}/.config/rclone/rclone.conf";
     owner = "${machine-config.username}";
-    group = pkgs.lib.mkIf (pkgs.lib.hasSuffix "darwin" pkgs.system) "${machine-config.username}";
+    group = pkgs.lib.mkIf (! pkgs.lib.hasSuffix "darwin" pkgs.system) "${machine-config.username}";
     mode = "400";
     symlink = false;
   };
