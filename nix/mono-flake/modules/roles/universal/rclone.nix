fc6ed921249de094ec65e3a7ea975a1d86a51532
ahilt <aidanhilt2@gmail.com>
2025-07-14 14:26:30 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/rclone.nix
+++ nix/mono-flake/modules/roles/universal/rclone.nix
@@ -81,54 +81,54 @@ in
     symlink = false;
   };
 
-  launchd.agents = pkgs.lib.mkIf (pkgs.lib.hasSuffix "darwin") {
-    rcloneKeepass = {
-      serviceConfig = {
-        UserName = "${machine-config.username}";
-        Program = /run/current-system/sw/bin/rclone;
-        ProgramArguments = [
-          "/run/current-system/sw/bin/rclone"
-          "bisync"
-          "drive:KeePass"
-          "/Users/${machine-config.username}/KeePass"
-          "--drive-skip-gdocs"
-          "--resilient"
-          "--create-empty-src-dirs"
-          "--fix-case"
-          "--slow-hash-sync-only"
-          "--config=/Users/${machine-config.username}/.config/rclone/rclone.conf"
-        ];
-        RunAtLoad = true;
-        StartInterval = 300;
-        StandardErrorPath = /tmp/rclone-keepass.log.err;
-        StandardOutPath = /tmp/rclone-keepass.log.out;
-      };
-    };
+  # launchd.agents = pkgs.lib.mkIf (pkgs.lib.hasSuffix "darwin") {
+  #   rcloneKeepass = {
+  #     serviceConfig = {
+  #       UserName = "${machine-config.username}";
+  #       Program = /run/current-system/sw/bin/rclone;
+  #       ProgramArguments = [
+  #         "/run/current-system/sw/bin/rclone"
+  #         "bisync"
+  #         "drive:KeePass"
+  #         "/Users/${machine-config.username}/KeePass"
+  #         "--drive-skip-gdocs"
+  #         "--resilient"
+  #         "--create-empty-src-dirs"
+  #         "--fix-case"
+  #         "--slow-hash-sync-only"
+  #         "--config=/Users/${machine-config.username}/.config/rclone/rclone.conf"
+  #       ];
+  #       RunAtLoad = true;
+  #       StartInterval = 300;
+  #       StandardErrorPath = /tmp/rclone-keepass.log.err;
+  #       StandardOutPath = /tmp/rclone-keepass.log.out;
+  #     };
+  #   };
 
 
-    rcloneWallpapers = {
-      serviceConfig = {
-        UserName = "${machine-config.username}";
-        Program = /run/current-system/sw/bin/rclone;
-        ProgramArguments = [
-          "/run/current-system/sw/bin/rclone"
-          "bisync"
-          "drive:Wallpapers"
-          "/Users/${machine-config.username}/Wallpapers"
-          "--drive-skip-gdocs"
-          "--resilient"
-          "--create-empty-src-dirs"
-          "--fix-case"
-          "--slow-hash-sync-only"
-          "--config=/Users/${machine-config.username}/.config/rclone/rclone.conf"
-        ];
-        RunAtLoad = true;
-        StartInterval = 300;
-        StandardErrorPath = /tmp/rclone-wallpapers.log.err;
-        StandardOutPath = /tmp/rclone-wallpapers.log.out;
-      };
-    };
-  };
+  #   rcloneWallpapers = {
+  #     serviceConfig = {
+  #       UserName = "${machine-config.username}";
+  #       Program = /run/current-system/sw/bin/rclone;
+  #       ProgramArguments = [
+  #         "/run/current-system/sw/bin/rclone"
+  #         "bisync"
+  #         "drive:Wallpapers"
+  #         "/Users/${machine-config.username}/Wallpapers"
+  #         "--drive-skip-gdocs"
+  #         "--resilient"
+  #         "--create-empty-src-dirs"
+  #         "--fix-case"
+  #         "--slow-hash-sync-only"
+  #         "--config=/Users/${machine-config.username}/.config/rclone/rclone.conf"
+  #       ];
+  #       RunAtLoad = true;
+  #       StartInterval = 300;
+  #       StandardErrorPath = /tmp/rclone-wallpapers.log.err;
+  #       StandardOutPath = /tmp/rclone-wallpapers.log.out;
+  #     };
+  #   };
+  # };
 
   # systemd = pkgs.lib.mkIf (! pkgs.lib.hasSuffix "darwin") {
   #   timers = {
