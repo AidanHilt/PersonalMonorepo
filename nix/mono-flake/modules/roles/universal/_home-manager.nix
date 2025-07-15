a1d483155f3ea6e8b5c70a6bd64d87c076d3161e
ahilt <aidanhilt2@gmail.com>
2025-07-14 23:59:26 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/_home-manager.nix
+++ nix/mono-flake/modules/roles/universal/_home-manager.nix
@@ -1,11 +1,15 @@
 { inputs, globals, pkgs, machine-config, ...}:
 
+let 
+  importingDir = builtins.toString (builtins.dirOf (__curPos.file or ./.));
+in
+
 {
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
     backupFileExtension = "bak";
     extraSpecialArgs = { inherit inputs globals pkgs machine-config; };
-    users.${machine-config.username} = import ./home.nix {inherit inputs globals pkgs machine-config; system = pkgs.system; lib = inputs.home-manager.lib; };
+    users.${machine-config.username} = import "${importing-dir}/home.nix" {inherit inputs globals pkgs machine-config; system = pkgs.system; lib = inputs.home-manager.lib; };
   };
 }
