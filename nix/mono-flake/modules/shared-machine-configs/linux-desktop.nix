5529929698a5c83271a0035760ae37bea1a852b1
ahilt <aidanhilt2@gmail.com>
2025-07-14 23:25:22 -0400

    Nix commit

--- nix/mono-flake/modules/shared-machine-configs/linux-desktop.nix
+++ nix/mono-flake/modules/shared-machine-configs/linux-desktop.nix
@@ -3,9 +3,12 @@
 {
   imports = [
     ../roles/nixos/linux-universal.nix
+
     ../roles/nixos/desktop/plasma-desktop.nix
-    ../roles/nixos/desktop/apps/virt-manager.nix
+
     ../roles/nixos/desktop/apps/keepassxc.nix
+    ../roles/nixos/desktop/apps/piper.nix
+    ../roles/nixos/desktop/apps/virt-manager.nix
 
     ../roles/universal/development-machine.nix
     ../roles/universal/linux-admin.nix
