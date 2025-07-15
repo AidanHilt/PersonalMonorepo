1544ff01426f7eff8f070222ca806ed74522e3ef
ahilt <aidanhilt2@gmail.com>
2025-07-15 00:03:38 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/_home-manager.nix
+++ nix/mono-flake/modules/roles/universal/_home-manager.nix
@@ -11,6 +11,6 @@ in
     useUserPackages = true;
     backupFileExtension = "bak";
     extraSpecialArgs = { inherit inputs globals pkgs machine-config; };
-    users.${machine-config.username} = import "${importingDir}/home.nix" {inherit inputs globals pkgs machine-config; system = pkgs.system; lib = inputs.home-manager.lib; };
+    users.${machine-config.username} = import "${callerDir}/home.nix" {inherit inputs globals pkgs machine-config; system = pkgs.system; lib = inputs.home-manager.lib; };
   };
 }
