007f606193d887c0dc85f8f58952e2e84d45a420
ahilt <aidanhilt2@gmail.com>
2025-07-19 10:43:45 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/_rclone-service-linux.nix
+++ nix/mono-flake/modules/roles/universal/_rclone-service-linux.nix
@@ -3,41 +3,41 @@
 lib.mkIf (pkgs.stdenv.isLinux) {
   systemd = {
     timers = {
-      # wallpaper-sync = {
-      #   wantedBy = [ "timers.target" ];
-      #   timerConfig = {
-      #     OnBootSec = "5m";
-      #     OnUnitActiveSec = "5m";
-      #     Unit = "wallpaper-sync.service";
-      #   };
-      # };
+      wallpaper-sync = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnBootSec = "5m";
+          OnUnitActiveSec = "5m";
+          Unit = "wallpaper-sync.service";
+        };
+      };
 
-      # keepass-sync = {
-      #   wantedBy = [ "timers.target" ];
-      #   timerConfig = {
-      #     OnBootSec = "5m";
-      #     OnUnitActiveSec = "5m";
-      #     Unit = "keepass-sync.service";
-      #   };
-      # };
+      keepass-sync = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnBootSec = "5m";
+          OnUnitActiveSec = "5m";
+          Unit = "keepass-sync.service";
+        };
+      };
 
-      # documents-folder-sync = {
-      #   wantedBy = [ "timers.target" ];
-      #   timerConfig = {
-      #     OnBootSec = "30m";
-      #     OnUnitActiveSec = "30m";
-      #     Unit = "documents-folder-sync.service";
-      #   };
-      # };
+      documents-folder-sync = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnBootSec = "30m";
+          OnUnitActiveSec = "30m";
+          Unit = "documents-folder-sync.service";
+        };
+      };
 
-      # lg-ghub-sync = {
-      #   wantedBy = [ "timers.target" ];
-      #   timerConfig = {
-      #     OnBootSec = "30m";
-      #     OnUnitActiveSec = "30m";
-      #     Unit = "lg-ghub-sync.service";
-      #   };
-      # };
+      lg-ghub-sync = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnBootSec = "30m";
+          OnUnitActiveSec = "30m";
+          Unit = "lg-ghub-sync.service";
+        };
+      };
     };
 
     services = {
@@ -53,41 +53,41 @@ lib.mkIf (pkgs.stdenv.isLinux) {
         };
       };
 
-      # keepass-sync = {
-      #   script = ''
-      #     set -xe
-      #     ${pkgs.rclone}/bin/rclone bisync drive:KeePass $KEEPASS_DIR --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync --config /home/${machine-config.username}/.config/rclone/rclone.conf
-      #   '';
+      keepass-sync = {
+        script = ''
+          set -xe
+          ${pkgs.rclone}/bin/rclone bisync drive:KeePass $KEEPASS_DIR --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync --config /home/${machine-config.username}/.config/rclone/rclone.conf
+        '';
 
-      #   serviceConfig = {
-      #     Type = "oneshot";
-      #     User = "root";
-      #   };
-      # };
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+        };
+      };
 
-      # documents-folder-sync = {
-      #   script = ''
-      #     set -xe
-      #     ${pkgs.rclone}/bin/rclone sync $WINDOWS_DOCUMENTS_DIR drive:Documents --drive-skip-gdocs --create-empty-src-dirs --fix-case --config /home/${machine-config.username}/.config/rclone/rclone.conf
-      #   '';
+      documents-folder-sync = {
+        script = ''
+          set -xe
+          ${pkgs.rclone}/bin/rclone sync $WINDOWS_DOCUMENTS_DIR drive:Documents --drive-skip-gdocs --create-empty-src-dirs --fix-case --config /home/${machine-config.username}/.config/rclone/rclone.conf
+        '';
 
-      #   serviceConfig = {
-      #     Type = "oneshot";
-      #     User = "root";
-      #   };
-      # };
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+        };
+      };
 
-      # lg-ghub-sync = {
-      #   script = ''
-      #     set -xe
-      #     ${pkgs.rclone}/bin/rclone sync $WINDOWS_GHUB_CONFIG_DIR drive:GHUB-Windows--drive-skip-gdocs --create-empty-src-dirs --fix-case --config /home/${machine-config.username}/.config/rclone/rclone.conf
-      #   '';
+      lg-ghub-sync = {
+        script = ''
+          set -xe
+          ${pkgs.rclone}/bin/rclone sync $WINDOWS_GHUB_CONFIG_DIR drive:GHUB-Windows--drive-skip-gdocs --create-empty-src-dirs --fix-case --config /home/${machine-config.username}/.config/rclone/rclone.conf
+        '';
 
-      #   serviceConfig = {
-      #     Type = "oneshot";
-      #     User = "root";
-      #   };
-      # };
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+        };
+      };
     };
   };
 }
