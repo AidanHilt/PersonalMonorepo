373d7f6fe5017a00e043dd1bac04d33fee72dff4
ahilt <aidanhilt2@gmail.com>
2025-07-19 10:49:37 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/rclone.nix
+++ nix/mono-flake/modules/roles/universal/rclone.nix
@@ -52,10 +52,6 @@ let
 in
 
 {
-  imports = [
-    ./_rclone-service-linux.nix
-  ];
-
   environment.systemPackages = with pkgs; [
     syncDownloadAll
     syncKeepass
@@ -79,5 +75,96 @@ in
     group = pkgs.lib.mkIf (! pkgs.lib.hasSuffix "darwin" pkgs.system) "${machine-config.username}";
     mode = "400";
     symlink = false;
+  };
+
+  lib.mkIf (pkgs.stdenv.isLinux) {
+    systemd = {
+      timers = {
+        wallpaper-sync = {
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnBootSec = "5m";
+            OnUnitActiveSec = "5m";
+            Unit = "wallpaper-sync.service";
+          };
+        };
+
+        keepass-sync = {
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnBootSec = "5m";
+            OnUnitActiveSec = "5m";
+            Unit = "keepass-sync.service";
+          };
+        };
+
+        documents-folder-sync = {
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnBootSec = "30m";
+            OnUnitActiveSec = "30m";
+            Unit = "documents-folder-sync.service";
+          };
+        };
+
+        lg-ghub-sync = {
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnBootSec = "30m";
+            OnUnitActiveSec = "30m";
+            Unit = "lg-ghub-sync.service";
+          };
+        };
+      };
+
+      services = {
+        wallpaper-sync = {
+          script = ''
+            set -xe
+            ${pkgs.rclone}/bin/rclone bisync drive:Wallpapers $WALLPAPER_DIR --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync --config /home/${machine-config.username}/.config/rclone/rclone.conf
+          '';
+
+          serviceConfig = {
+            Type = "oneshot";
+            User = "root";
+          };
+        };
+
+        keepass-sync = {
+          script = ''
+            set -xe
+            ${pkgs.rclone}/bin/rclone bisync drive:KeePass $KEEPASS_DIR --drive-skip-gdocs --resilient --create-empty-src-dirs --fix-case --slow-hash-sync-only --resync --config /home/${machine-config.username}/.config/rclone/rclone.conf
+          '';
+
+          serviceConfig = {
+            Type = "oneshot";
+            User = "root";
+          };
+        };
+
+        documents-folder-sync = {
+          script = ''
+            set -xe
+            ${pkgs.rclone}/bin/rclone sync $WINDOWS_DOCUMENTS_DIR drive:Documents --drive-skip-gdocs --create-empty-src-dirs --fix-case --config /home/${machine-config.username}/.config/rclone/rclone.conf
+          '';
+
+          serviceConfig = {
+            Type = "oneshot";
+            User = "root";
+          };
+        };
+
+        lg-ghub-sync = {
+          script = ''
+            set -xe
+            ${pkgs.rclone}/bin/rclone sync $WINDOWS_GHUB_CONFIG_DIR drive:GHUB-Windows--drive-skip-gdocs --create-empty-src-dirs --fix-case --config /home/${machine-config.username}/.config/rclone/rclone.conf
+          '';
+
+          serviceConfig = {
+            Type = "oneshot";
+            User = "root";
+          };
+        };
+    };
   };
 }
