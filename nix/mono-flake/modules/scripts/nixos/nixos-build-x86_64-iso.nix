79ddcbb1201a436dd26317b13c13299513943f7d
ahilt <aidanhilt2@gmail.com>
2025-07-19 01:17:06 -0400

    Nix commit

---
+++ nix/mono-flake/modules/scripts/nixos/nixos-build-x86_64-iso.nix
@@ -0,0 +1,16 @@
+{ inputs, globals, pkgs, machine-config, ...}:
+
+let
+nixos-build-x86_64-iso = pkgs.writeShellScriptBin "nixos-build-x86_64-iso" ''
+#!/bin/bash
+
+set -euo pipefail
+nix build $PERSONAL_MONOREPO_LOCATION/nix/mono-flake#nixosConfigurations.iso-image-x86_64.config.system.build.isoImage
+'';
+in
+
+{
+  environment.systemPackages = [
+    nixos-build-x86_64-iso
+  ];
+}
