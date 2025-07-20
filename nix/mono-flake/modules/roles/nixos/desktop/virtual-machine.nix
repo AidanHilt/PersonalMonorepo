ed4f9af88153aa3539772991e1ba4793d17e6e8e
ahilt <aidanhilt2@gmail.com>
2025-07-20 09:27:13 -0400

    Nix commit

--- nix/mono-flake/modules/roles/nixos/desktop/virtual-machine.nix
+++ nix/mono-flake/modules/roles/nixos/desktop/virtual-machine.nix
@@ -7,9 +7,18 @@
     virt-manager
   ];
 
+  networking = {
+    # Create the bridge interface
+    bridges = {
+      "br0" = {
+        interfaces = [ "enp4s0" ];  # Replace with your physical interface name
+      };
+    };
+  };
+
   virtualisation.libvirtd = {
     enable = true;
-    allowedBridges = ["enp4s0"];
+    allowedBridges = ["br0"];
   };
 
   users.users.${machine-config.username}.extraGroups = [ "libvirtd" ];
