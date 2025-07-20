c410251036a1d171315c91c4b602b90dbd3203cc
ahilt <aidanhilt2@gmail.com>
2025-07-20 09:23:32 -0400

    Nix commit

--- nix/mono-flake/modules/roles/nixos/desktop/virtual-machine.nix
+++ nix/mono-flake/modules/roles/nixos/desktop/virtual-machine.nix
@@ -1,12 +1,16 @@
 { inputs, globals, pkgs, machine-config, lib, ...}:
 
 {
-  virtualisation.libvirtd.enable = true;
-  users.users.${machine-config.username}.extraGroups = [ "libvirtd" ];
-
   environment.systemPackages = with pkgs; [
     qemu
     vagrant
     virt-manager
   ];
+
+  virtualisation.libvirtd = {
+    enable = true;
+    allowedBridges = ["enp4s0"];
+  };
+
+  users.users.${machine-config.username}.extraGroups = [ "libvirtd" ];
 }
