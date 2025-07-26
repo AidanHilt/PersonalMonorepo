e2a4ace7a5cfc973365d740fa69c3f06a77c72b4
ahilt <aidanhilt2@gmail.com>
2025-07-26 14:00:36 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/kubernetes-admin.nix
+++ nix/mono-flake/modules/roles/universal/kubernetes-admin.nix
@@ -166,6 +166,7 @@ in
     # REASON We need to access config, which is not available in the let statement
     (pkgs.writeShellScriptBin "sync-kubeconfig" ''
       KUBECONFIG=''${KUBECONFIG:-$HOME/.kube/config}
+      mkdir $(dirname $KUBECONFIG)
       cp ${config.age.secrets.kubeconfig.path} $KUBECONFIG
       chmod 600 $KUBECONFIG
     '')
