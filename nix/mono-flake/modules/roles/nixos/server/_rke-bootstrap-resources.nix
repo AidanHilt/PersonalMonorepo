2d151476c337409ce21ca0a26a45c42eb31c2de0
ahilt <aidanhilt2@gmail.com>
2025-07-27 17:50:41 -0400

    Nix commit

--- nix/mono-flake/modules/roles/nixos/server/_rke-bootstrap-resources.nix
+++ nix/mono-flake/modules/roles/nixos/server/_rke-bootstrap-resources.nix
@@ -1,7 +1,7 @@
 { inputs, globals, pkgs, machine-config, lib, ...}:
 
 let 
-  argocdManifest = pkgs.writeText "argocd-helm.yaml" "test";
+  argocdManifest = pkgs.writeText "argocd-helm.yaml" "stest";
 
   manifestPath = "/var/lib/rancher/rke2/server/manifests";
 in
