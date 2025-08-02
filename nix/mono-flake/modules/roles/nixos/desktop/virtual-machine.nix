07494a2feeaaa9ac4b56c759bcf73774143f4432
ahilt <aidanhilt2@gmail.com>
2025-08-02 00:02:45 -0400

    Nix commit

--- nix/mono-flake/modules/roles/nixos/desktop/virtual-machine.nix
+++ nix/mono-flake/modules/roles/nixos/desktop/virtual-machine.nix
@@ -6,10 +6,11 @@ let
   networkingConfig = if isFixedIp then {
     # useNetworkd = true;
     # networkmanager.enable = lib.mkForce false;
-    interfaces.${machine-config.networking.mainNetworkInterface}.useDHCP = false;
     interfaces.br0.useDHCP = false;
 
-    interfaces.${machine-config.networking.mainNetworkInterface}.ipv4.addresses = lib.mkForce null;
+    interfaces.${machine-config.networking.mainNetworkInterface} = lib.mkForce {
+      useDHCP = false;
+    };
 
     interfaces.br0.ipv4.addresses = [
       {
@@ -49,18 +50,7 @@ in
     LIBVIRT_DEFAULT_URI="qemu:///system";
   };
 
-  networking = {
-    # useNetworkd = true;
-    # networkmanager.enable = lib.mkForce false;
-    interfaces.${machine-config.networking.mainNetworkInterface}.useDHCP = false;
-    interfaces.br0.useDHCP = true;
-   
-    bridges = {
-      "br0" = {
-        interfaces = [ "${machine-config.networking.mainNetworkInterface}" ];
-      };
-    };
-  };
+  networking = networkingConfig;
 
   virtualisation.libvirtd = {
     enable = true;
