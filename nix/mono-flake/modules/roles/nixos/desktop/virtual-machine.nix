bf5de80603f2d60b5ccd62a90ddc06438534edca
ahilt <aidanhilt2@gmail.com>
2025-08-01 23:59:24 -0400

    Nix commit

--- nix/mono-flake/modules/roles/nixos/desktop/virtual-machine.nix
+++ nix/mono-flake/modules/roles/nixos/desktop/virtual-machine.nix
@@ -1,5 +1,44 @@
 { inputs, globals, pkgs, machine-config, lib, ...}:
 
+let
+  isFixedIp = machine-config.networking.fixedIp or false;
+
+  networkingConfig = if isFixedIp then {
+    # useNetworkd = true;
+    # networkmanager.enable = lib.mkForce false;
+    interfaces.${machine-config.networking.mainNetworkInterface}.useDHCP = false;
+    interfaces.br0.useDHCP = false;
+
+    interfaces.${machine-config.networking.mainNetworkInterface}.ipv4.addresses = lib.mkForce null;
+
+    interfaces.br0.ipv4.addresses = [
+      {
+        address = machine-config.networking.address;
+        prefixLength = machine-config.networking.prefixLength;
+      }
+    ];
+   
+    bridges = {
+      "br0" = {
+        interfaces = [ "${machine-config.networking.mainNetworkInterface}" ];
+      };
+    };
+  }
+  
+  else {
+    # useNetworkd = true;
+    # networkmanager.enable = lib.mkForce false;
+    interfaces.${machine-config.networking.mainNetworkInterface}.useDHCP = false;
+    interfaces.br0.useDHCP = true;
+   
+    bridges = {
+      "br0" = {
+        interfaces = [ "${machine-config.networking.mainNetworkInterface}" ];
+      };
+    };
+  };
+in
+
 {
   environment.systemPackages = with pkgs; [
     qemu
