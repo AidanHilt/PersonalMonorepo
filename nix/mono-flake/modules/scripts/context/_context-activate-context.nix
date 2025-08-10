a926652a578bed534c18b0097647fbec14dc1285
aidan <aidanhilt2@gmail.com>
2025-08-10 15:32:00 -0400

    Nix commit

--- nix/mono-flake/modules/scripts/context/_context-activate-context.nix
+++ nix/mono-flake/modules/scripts/context/_context-activate-context.nix
@@ -1,15 +1,7 @@
 { inputs, globals, pkgs, machine-config, lib, ...}:
 
-let
-_context-activate-context = pkgs.writeShellScriptBin "_context-activate-context" ''
-#!/bin/bash
-
-set -euo pipefail
-'';
-in
-
 {
-  environment.systemPackages = [
-    _context-activate-context
-  ];
+  environment.shellInit = ''
+
+  '';
 }
