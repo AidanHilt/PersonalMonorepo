1a2f0a736e16803bee5f2d2954776091e90caf69
aidan <aidanhilt2@gmail.com>
2025-08-11 23:40:40 -0400

    Nix commit

--- nix/mono-flake/modules/scripts/context/_context-activate-context.nix
+++ nix/mono-flake/modules/scripts/context/_context-activate-context.nix
@@ -5,40 +5,39 @@ let
   contextSelector = import ./_context-context-selector.nix {inherit pkgs;};
 
   activateContext = pkgs.writeText "activate-context.sh" ''
-    context-activate-context () {
-    CONTEXT_NAME=""
+      context-activate-context () {
+        CONTEXT_NAME=""
 
-    while [[ $# -gt 0 ]]; do
-      case $1 in
-        --name|-n)
-          CONTEXT_NAME="$2"
-          shift 2
-          ;;
-        --help|-h)
-          echo "Usage: $0 --context <context_name>"
-          echo "     $0 -c <context_name>"
-          echo
-          echo "Options:"
-          echo "  --name, -n  Name of the context to activate"
-          echo "  --help, -h     Show this help message"
-          echo
-          exit 0
-          ;;
-        *)
-          echo "Error: Unknown option $1"
-          show_usage
-          exit 1
-          ;;
-      esac
-    done
+        while [[ $# -gt 0 ]]; do
+          case $1 in
+            --name|-n)
+              CONTEXT_NAME="$2"
+              shift 2
+              ;;
+            --help|-h)
+              echo "Usage: $0 --context <context_name>"
+              echo "     $0 -c <context_name>"
+              echo
+              echo "Options:"
+              echo "  --name, -n  Name of the context to activate"
+              echo "  --help, -h     Show this help message"
+              echo
+              exit 0
+              ;;
+            *)
+              echo "Error: Unknown option $1"
+              show_usage
+              exit 1
+              ;;
+          esac
+        done
 
-    if [[ -z "$CONTEXT_NAME" ]]; then
-      _context-context-selector
-    fi
+        if [[ -z "$CONTEXT_NAME" ]]; then
+          _context-context-selector
+        fi
 
-    eval "$(dotenvx get -f "$ATILS_CONTEXTS_DIRECTORY/$CONTEXT_NAME/.env" | tr -d '}' | tr -d '{' | sed 's/:/=/g' | sed 's/,/\n/g' | sed 's/^/export /')"
-
-
+        eval "$(dotenvx get -f "$ATILS_CONTEXTS_DIRECTORY/$CONTEXT_NAME/.env" | tr -d '}' | tr -d '{' | sed 's/:/=/g' | sed 's/,/\n/g' | sed 's/^/export /')"
+      }
   '';
 in
 
