594aadd72c05168df7e43546ea99bbaf78a72ee0
aidan <aidanhilt2@gmail.com>
2025-08-19 23:22:21 -0400

    Nix commit

--- nix/mono-flake/modules/scripts/vault/vault-provision.nix
+++ nix/mono-flake/modules/scripts/vault/vault-provision.nix
@@ -10,17 +10,17 @@ set -euo pipefail
 
 source ${printing-and-output.printing-and-output}
 
-valid_run=false
+valid_run="false"
 
 if [[ ! -v TG_WORKING_DIR ]]; then
-  valid_run=true
+  valid_run="true"
 fi
 
 if [[ -f vault/terragrunt.hcl ]] || [[ -f terragrunt.hcl ]]; then
-  valid_run=true
+  valid_run="true"
 fi
 
-if valid_run=false; then
+if valid_run="false"; then
   print_error 'Please set TG_WORKING_DIR, or navigate to $TG_WORKING_DIR or $TG_WORKING_DIR/vault'
 fi
 '';
