14885429e14345ec2467d5c70c50325f5b7b19e6
aidan <aidanhilt2@gmail.com>
2025-08-20 00:09:18 -0400

    Nix commit

--- nix/mono-flake/modules/scripts/vault/vault-provision.nix
+++ nix/mono-flake/modules/scripts/vault/vault-provision.nix
@@ -12,19 +12,16 @@ source ${printing-and-output.printing-and-output}
 
 valid_run="false"
 
-if [[ -v TG_WORKING_DIR ]] || [[ -f vault/terragrunt.hcl ]] || [[ -f terragrunt.hcl ]]; then
+if [[ -v TG_WORKING_DIR ]]; then
   valid_run="true"
 fi
 
 if [[ "$valid_run" == "false" ]]; then
-  print_error 'Please set TG_WORKING_DIR, or navigate to $TG_WORKING_DIR or $TG_WORKING_DIR/vault'
+  print_error 'Please set TG_WORKING_DIR'
 fi
 
-if [[ -f vault/terragrunt.hcl ]] || [[ -v TG_WORKING_DIR ]]; then
-  terragrunt apply -terragrunt-include-dirs "''${TG_WORKING_DIR}/vault/*"
-else
-  terragrunt apply
-fi
+echo "$VAULT_TOKEN"
+terragrunt run apply --all --working-dir "$TG_WORKING_DIR/vault"
 '';
 in
 
