de0e388512001ed13c071029c0bcb218dc7154cc
Aidan Hilt <ahilt@uchicago.edu>
2025-09-04 14:30:48 -0400

    Nix commit

--- nix/mono-flake/lib-internal/packages.nix
+++ nix/mono-flake/lib-internal/packages.nix
@@ -4,6 +4,8 @@ let
   lib = nixpkgs.lib;
 
   discovery = import ./discovery.nix { inherit nixpkgs darwin inputs; };
+
+  pkgs = import inputs.nixpkgs;
 in
 
 {
@@ -15,8 +17,21 @@ in
         systemOverlays = platformOverlays.${system} or [];
         patches = [
           (final: prev: {
+            terragrunt = prev.terragrunt.overrideAttrs (oldAttrs: {
+              pname = "terragrunt";
+              version = "0.45.17";
+              vendorHash = "sha256-l9yGP6+Sc1RiRDbtPWI3X07KnUNm7z8Mfe0F89q5B2Y=";
+
+              src = prev.fetchFromGitHub {
+                owner = "gruntwork-io";
+                repo = "terragrunt";
+                rev = "v0.45.17";
+                sha256 = "sha256-mNrRzXj9bcHr8gATa7O4nSxCA6AJeM4TdyMj7GB4QMo=";
+              };
+            });
+
             grub2 = prev.grub2.overrideAttrs (oldAttrs: {
-              patches = map (patch: 
+              patches = map (patch:
                 if patch.name or "" == "23_prerequisite_1_key_protector_add_key_protectors_framework.patch"
                   then patch // { hash = "sha256-5aFHzc5qXBNLEc6yzI17AH6J7EYogcXdLxk//1QgumY="; }
                 else if patch.name or "" == "23_CVE-2024-49504.patch"
