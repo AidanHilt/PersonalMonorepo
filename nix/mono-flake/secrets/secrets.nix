65b23fb7be5142d0c7fed7e6105e6296f8f45e3c
ahilt <aidanhilt2@gmail.com>
2025-09-30 09:56:22 -0400

    Nix commit

--- nix/mono-flake/secrets/secrets.nix
+++ nix/mono-flake/secrets/secrets.nix
@@ -33,7 +33,7 @@ let
   # Our main staging cluster, in the form of NixOS machines running on x86 hardware
   staging-cluster-1-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHD8vp3GixOYJd3zCUnGTwFMB3lGPZ16g9gsB6+Y/7wb noname";
   staging-cluster-2-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPnHw1P4iuDZSYkuaMPqxh28yTFxGkIiRo6/t/+FlC3 noname";
-  staging-cluster-3-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEoA+NyIwcFrj/INIqVRnD4Bg/lUUhe2xDObzqRHvaYR noname";
+  staging-cluster-3-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKlIyOATYZgX8Z3BYVjmwepbFFjSGf9kmh/bF+CcVpr noname";
 
   staging-cluster-machines = [staging-cluster-1-system staging-cluster-2-system staging-cluster-3-system];
 in
