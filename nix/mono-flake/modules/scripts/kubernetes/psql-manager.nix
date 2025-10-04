6621caf5f12c307be52ca6b196c4a40ef2cf2944
aidan <aidanhilt2@gmail.com>
2025-10-03 23:43:15 -0400

    Nix commit

--- nix/mono-flake/modules/scripts/kubernetes/psql-manager.nix
+++ nix/mono-flake/modules/scripts/kubernetes/psql-manager.nix
@@ -79,6 +79,11 @@ print_status "Connecting to database ''${DATABASE} at ''${POSTGRES_ENDPOINT}"
 kubectl exec -it "''${POD_NAME}" -n "''${NAMESPACE}" -- \
   psql -h "''${POSTGRES_ENDPOINT}" -U "''${USERNAME}" -d "''${DATABASE}" || true
 
+if [[ $? != 0 ]]; then
+  print_warning "Was not able to connect directly, dropping into a bash shell to allow manual retries"
+  kubectl exec -it "''${POD_NAME}" -n "''${NAMESPACE}" -- sh || true
+fi
+
 print_status "Cleaning up pod ''${POD_NAME}"
 kubectl delete pod "''${POD_NAME}" -n "''${NAMESPACE}" --wait=false
 '';
