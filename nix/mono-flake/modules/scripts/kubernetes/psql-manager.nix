6e74d4443ec0370bdb790717f2cc3c82766c3d9e
aidan <aidanhilt2@gmail.com>
2025-10-03 23:29:06 -0400

    Nix commit

--- nix/mono-flake/modules/scripts/kubernetes/psql-manager.nix
+++ nix/mono-flake/modules/scripts/kubernetes/psql-manager.nix
@@ -9,6 +9,78 @@ psql-manager = pkgs.writeShellScriptBin "psql-manager" ''
 set -euo pipefail
 
 source ${printing-and-output.printing-and-output}
+
+SECRET_NAME="postgres-config"
+NAMESPACE="postgres"
+USERNAME_KEY="username"
+PASSWORD_KEY="password"
+POSTGRES_ENDPOINT="postgres-cluster-rw.postgres.cluster.svc.local"
+DATABASE="postgres"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --secret-name)
+      SECRET_NAME="$2"
+      shift 2
+      ;;
+    --namespace)
+      NAMESPACE="$2"
+      shift 2
+      ;;
+    --username-key)
+      USERNAME_KEY="$2"
+      shift 2
+      ;;
+    --password-key)
+      PASSWORD_KEY="$2"
+      shift 2
+      ;;
+    --postgres-endpoint)
+      POSTGRES_ENDPOINT="$2"
+      shift 2
+      ;;
+    --database)
+      DATABASE="$2"
+      shift 2
+      ;;
+    *)
+      print_error "Unknown argument: $1"
+      exit 1
+      ;;
+  esac
+done
+
+print_status "Reading secret ''${SECRET_NAME} from namespace ''${NAMESPACE}"
+
+USERNAME=$(kubectl get secret "''${SECRET_NAME}" -n "''${NAMESPACE}" -o jsonpath="{.data.''${USERNAME_KEY}}" | base64 -d)
+PASSWORD=$(kubectl get secret "''${SECRET_NAME}" -n "''${NAMESPACE}" -o jsonpath="{.data.''${PASSWORD_KEY}}" | base64 -d)
+
+if [ -z "''${USERNAME}" ] || [ -z "''${PASSWORD}" ]; then
+  print_error "Failed to retrieve credentials from secret"
+  exit 1
+fi
+
+POD_NAME="psql-client-''${RANDOM}"
+
+print_status "Launching psql client pod: ''${POD_NAME}"
+
+kubectl run "''${POD_NAME}" \
+  -n "''${NAMESPACE}" \
+  --image=atils-postgres-client:latest \
+  --restart=Never \
+  --env="PGPASSWORD=''${PASSWORD}" \
+  --command -- sleep infinity
+
+print_status "Waiting for pod to be ready"
+kubectl wait --for=condition=ready pod/"''${POD_NAME}" -n "''${NAMESPACE}" --timeout=60s
+
+print_status "Connecting to database ''${DATABASE} at ''${POSTGRES_ENDPOINT}"
+
+kubectl exec -it "''${POD_NAME}" -n "''${NAMESPACE}" -- \
+  psql -h "''${POSTGRES_ENDPOINT}" -U "''${USERNAME}" -d "''${DATABASE}"
+
+print_status "Cleaning up pod ''${POD_NAME}"
+kubectl delete pod "''${POD_NAME}" -n "''${NAMESPACE}" --wait=false
 '';
 in
 
