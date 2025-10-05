5b45af8db951ca731e9c24c24415d186df2b0b8e
aidan <aidanhilt2@gmail.com>
2025-10-05 11:08:38 -0400

    Nix commit

--- nix/mono-flake/machines/x86_64-linux/laptop-node/disko.nix
+++ nix/mono-flake/machines/x86_64-linux/laptop-node/disko.nix
@@ -1,41 +1,39 @@
 { inputs, globals, pkgs, system, machine-config, ...}:
 
 {
-  {
-    disko.devices = {
-      disk = {
-        main = {
-          device = "/dev/disk/by-uuid/85bd05ad-81eb-4109-add4-04c7e085d78f";
-          type = "disk";
-          content = {
-            type = "gpt";
-            partitions = {
-              boot = {
-                size = "1M";
-                type = "EF02"; # for grub MBR
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/disk/by-uuid/85bd05ad-81eb-4109-add4-04c7e085d78f";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
-              ESP = {
-                size = "1G";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot";
-                  mountOptions = [ "umask=0077" ];
-                };
-              };
-              root = {
-                size = "100%";
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
-                };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
           };
         };
       };
     };
-  }
+  };
 }
