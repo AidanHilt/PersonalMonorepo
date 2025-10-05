4dfdfec623d31792ef165694aadd370a6a570c47
aidan <aidanhilt2@gmail.com>
2025-10-05 01:19:49 -0400

    Ugh

---
+++ nix/mono-flake/modules/shared-values/prod-cluster.nix
@@ -0,0 +1,18 @@
+{
+  username = "aidan";
+
+  networking = {
+    fixedIp = true;
+
+    defaultGateway = "192.168.86.1";
+    nameservers = [ "192.168.86.3" ];
+    prefixLength = 24;
+
+    loadBalancerIp = "192.168.86.22";
+    mainNetworkInterface = "ens3";
+  };
+
+  k8s = {
+    clusterName = "prod-cluster";
+  };
+}
