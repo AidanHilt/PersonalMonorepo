352a7b36e8c8da0898aca1bf8e635c0090daab31
aidan <aidanhilt2@gmail.com>
2025-10-05 10:56:45 -0400

    Nix commit

--- nix/mono-flake/secrets/secrets.nix
+++ nix/mono-flake/secrets/secrets.nix
@@ -40,6 +40,7 @@ let
   # Prod cluster! These are x86 machines (for now) running NixOS. Their config should closely match the staging cluster
 
   prod-cluster-machines = [];
+  laptop-node-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAUuOl807rzUB5yWyIm3HJ2XuwpBes/rdQSdo+W63NiQ noname;"
 in
 {
   "hosts.age".publicKeys = user-machines ++ laptop-cluster-machines;
