d217ac426f0007cd2ccc6b0d38408798e35ae181
aidan <aidanhilt2@gmail.com>
2025-10-05 11:07:37 -0400

    Nix commit

--- nix/mono-flake/machines/x86_64-linux/laptop-node/disko.nix
+++ nix/mono-flake/machines/x86_64-linux/laptop-node/disko.nix
@@ -1,31 +1,41 @@
 { inputs, globals, pkgs, system, machine-config, ...}:
 
 {
-  imports = [
-    # ../../../modules/shared-disko-configs/vda-single-disk.nix
-  ];
-
-  # disko.devices = {
-  #   disk = {
-  #     main = {
-  #       device = "";
-  #       type = "disk";
-  #       content = {
-  #         type = "";
-  #         partitions = {
-  #           ESP = {
-  #             size = "";
-  #             type = "";
-  #             content = {
-  #               type = "filesystem";
-  #               format = "";
-  #               mountpoint = "";
-  #               mountOptions = [ "umask=0077" ];
-  #             };
-  #           };
-  #         };
-  #       };
-  #     };
-  #   };
-  # };
+  {
+    disko.devices = {
+      disk = {
+        main = {
+          device = "/dev/disk/by-uuid/85bd05ad-81eb-4109-add4-04c7e085d78f";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                size = "1M";
+                type = "EF02"; # for grub MBR
+              };
+              ESP = {
+                size = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  }
 }
