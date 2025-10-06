46046f217c34d9fa644025aae4a5add11ffcd30e
aidan <aidanhilt2@gmail.com>
2025-10-06 16:08:54 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/_hosts.nix
+++ nix/mono-flake/modules/roles/universal/_hosts.nix
@@ -3,35 +3,35 @@
     # =================
     # Personal Machines
     # =================
-    "192.168.86.40" = ["big-boi-desktop.local"];
+    "192.168.86.40" = ["big-boi-desktop.lan"];
 
     # ===================
     # 3-node prod cluster
     # ===================
-    "192.168.86.4" = ["optiplex-node.local"];
-    "192.168.86.5" = ["gaming-pc-node.local"];
-    "192.168.86.6" = ["laptop-node.local"];
+    "192.168.86.4" = ["optiplex-node.lan"];
+    "192.168.86.5" = ["gaming-pc-node.lan"];
+    "192.168.86.6" = ["laptop-node.lan"];
 
     # =====================
     # 2-node laptop cluster
     # =====================
-    "192.168.86.20" = ["laptop-cluster-1.local"];
-    "192.168.86.21" = ["laptop-cluster-2.local"];
+    "192.168.86.20" = ["laptop-cluster-1.lan"];
+    "192.168.86.21" = ["laptop-cluster-2.lan"];
 
     # =========================
     # 3-node VM staging cluster
     # =========================
-    "192.168.86.24" = ["staging-cluster-1.local"];
-    "192.168.86.25" = ["staging-cluster-2.local"];
-    "192.168.86.26" = ["staging-cluster-3.local"];
+    "192.168.86.24" = ["staging-cluster-1.lan"];
+    "192.168.86.25" = ["staging-cluster-2.lan"];
+    "192.168.86.26" = ["staging-cluster-3.lan"];
   };
 
   wildcardEntries = {
-    "*.prod-cluster-node-lb.local" = "192.168.86.3";
-    "*.prod-cluster-lb.local" = "192.168.86.13";
-    "*.staging-cluster-node-lb.local" = "192.168.86.22";
-    "*.staging-cluster-lb.local" = "192.168.86.23";
-    "*.laptop-cluster-node-lb.local" = "192.168.86.18";
-    "*.laptop-cluster-lb.local" = "192.168.86.19";
+    "*.prod-cluster-node-lb.lan" = "192.168.86.3";
+    "*.prod-cluster-lb.lan" = "192.168.86.13";
+    "*.staging-cluster-node-lb.lan" = "192.168.86.22";
+    "*.staging-cluster-lb.lan" = "192.168.86.23";
+    "*.laptop-cluster-node-lb.lan" = "192.168.86.18";
+    "*.laptop-cluster-lb.lan" = "192.168.86.19";
   };
 }
