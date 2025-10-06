2b1371e9e29640c6c78c9e7d6f0d2786c38d8f60
aidan <aidanhilt2@gmail.com>
2025-10-05 20:52:37 -0400

    Nix commit

--- nix/mono-flake/modules/shared-values/prod-cluster.nix
+++ nix/mono-flake/modules/shared-values/prod-cluster.nix
@@ -5,7 +5,7 @@
     fixedIp = true;
 
     defaultGateway = "192.168.86.1";
-    nameservers = [ "192.168.86.13" ];
+    nameservers = [ "192.168.86.3" ];
     prefixLength = 24;
 
     loadBalancerIp = "192.168.86.13";
