d6d95ee485e06d7df2eafe3b17efc3b8c5020cd3
aidan <aidanhilt2@gmail.com>
2025-10-07 23:49:48 -0400

    Nix commit

--- nix/mono-flake/modules/roles/nixos/hosts.nix
+++ nix/mono-flake/modules/roles/nixos/hosts.nix
@@ -5,37 +5,7 @@ let
 
   # Convert hosts map to dnsmasq address entries
   # Each IP can have multiple hostnames
-  dnsmasqAddresses = lib.flatten (
-    lib.mapAttrsToList (ip: hostnames:
-      map (hostname: "address=/${hostname}/${ip}") hostnames
-    ) dnsConstants.dnsHosts
-  );
-
-    parseWildcardEntry = entry:
-    let
-      parts = builtins.match "address=/\\*\\.([^/]+)/([0-9.]+)" entry;
-    in
-    if parts != null then
-      { domain = builtins.elemAt parts 0; ip = builtins.elemAt parts 1; }
-    else
-      null;
-
-  groupByIp = entries:
-    let
-      parsed = builtins.filter (x: x != null) (map parseWildcardEntry entries);
-
-      addToGroups = groups: entry:
-        let
-          existingDomains = groups.${entry.ip} or [];
-        in
-        groups // {
-          entry.ip = existingDomains ++ [ entry.domain ];
-        };
-    in
-    builtins.foldl' addToGroups {} parsed;
-
-  wildcardEntriesToHosts = entries: groupByIp dnsConstants.wildcardEntries;
 in
 {
-  networking.hosts = dnsConstants.dnsHosts // wildcardEntriesToHosts{};
+  networking.hosts = dnsConstants.dnsHosts // dnsConstants.wildcardEntries;
 }
