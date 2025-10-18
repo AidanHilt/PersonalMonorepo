823789036896397e464206185162bb07b69f637d
aidan <aidanhilt2@gmail.com>
2025-10-18 14:12:52 -0400

    Nix commit

--- nix/mono-flake/modules/scripts/helm/helm-k8s-resource-new-stack.nix
+++ nix/mono-flake/modules/scripts/helm/helm-k8s-resource-new-stack.nix
@@ -9,6 +9,60 @@ helm-k8s-resource-new-stack = pkgs.writeShellScriptBin "helm-k8s-resource-new-st
 set -euo pipefail
 
 source ${printing-and-output.printing-and-output}
+
+OUTPUT_DIR=""
+CHART_NAME=""
+STACK_NAME=""
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --output-dir)
+      OUTPUT_DIR="$2"
+      shift 2
+      ;;
+    --chart-name)
+      CHART_NAME="$2"
+      shift 2
+      ;;
+    --stack-name)
+      STACK_NAME="$2"
+      shift 2
+      ;;
+    *)
+      print_error "Unknown argument: $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [[ -z "''${OUTPUT_DIR}" ]]; then
+  print_error "--output-dir must be provided"
+  exit 1
+fi
+
+if [[ -z "''${CHART_NAME}" ]]; then
+  CHART_NAME="$(select-directory)"
+fi
+
+SELECTED_DIR="''${OUTPUT_DIR}/''${CHART_NAME}"
+
+if [[ -z "''${STACK_NAME}" ]]; then
+  read -r -p "Enter stack name: " STACK_NAME
+fi
+
+print_status "Editing values.yaml for stack ''${STACK_NAME}"
+_edit-yaml-in-place "''${SELECTED_DIR}/values.yaml" "''${STACK_NAME}="
+
+TEMPLATE_SOURCE="''${PERSONAL_MONOREPO_LOCATION}/helm/templates/stack-template.yaml"
+TEMPLATE_DEST="''${SELECTED_DIR}/templates/''${STACK_NAME}.yaml"
+
+print_status "Copying template to ''${TEMPLATE_DEST}"
+cp "''${TEMPLATE_SOURCE}" "''${TEMPLATE_DEST}"
+
+export STACK_NAME
+envsubst < "''${TEMPLATE_DEST}" > "''${TEMPLATE_DEST}.tmp"
+mv "''${TEMPLATE_DEST}.tmp" "''${TEMPLATE_DEST}"
+
 '';
 in
 
--- nix/mono-flake/modules/scripts/lib/_edit-yaml-in-place.nix
+++ nix/mono-flake/modules/scripts/lib/_edit-yaml-in-place.nix
@@ -8,7 +8,21 @@ _edit-yaml-in-place = pkgs.writeShellScriptBin "_edit-yaml-in-place" ''
 
 set -euo pipefail
 
-source ${printing-and-output.printing-and-output}
+source ''${printing-and-output.printing-and-output}
+
+FILENAME="$1"
+YQ_STRING="$2"
+TMP_FILE="/tmp/$(basename "''${FILENAME}")"
+
+if [[ -z "''${FILENAME}" || -z "''${YQ_STRING}" ]]; then
+  print_error "Both filename and yq string must be provided"
+  exit 1
+fi
+
+print_status "Applying yq transformation to PRINT_DEBUGFILENAME}"
+
+yq "''${YQ_STRING}" "''${FILENAME}" > "''${TMP_FILE}"
+mv "''${TMP_FILE}" "''${FILENAME}"
 '';
 in
 
