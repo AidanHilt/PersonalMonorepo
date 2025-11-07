9c67154ac67f1f958aa3034cdc2613eec2e26d19
aidan <aidanhilt2@gmail.com>
2025-11-06 20:14:05 -0500

    Nix commit

--- nix/mono-flake/modules/scripts/app-creator/app-creator-add-terraform-secret.nix
+++ nix/mono-flake/modules/scripts/app-creator/app-creator-add-terraform-secret.nix
@@ -100,14 +100,14 @@ done
 LOCAL_FILE="''${PERSONAL_MONOREPO_LOCATION}/terraform/vault-config/locals.tf.json"
 print_debug "Updating locals.tf.json at $LOCAL_FILE"
 
-for entry in "‘''${SECRET_KEYS[@]}"; do
+for entry in "''${SECRET_KEYS[@]}"; do
   key_name="$(cut -d'|' -f1 <<< "$entry")"
   is_pg_password="$(cut -d'|' -f2 <<< "$entry")"
   key_value="$(cut -d'|' -f3 <<< "$entry")"
-  HCLEDIT_PATH=".locals.secret_definitions.‘''${SECRET_NAME}.data.‘''${key_name}"
+  HCLEDIT_PATH=".locals.secret_definitions.''${SECRET_NAME}.data.''${key_name}"
   jq "$LOCAL_FILE" "$HCLEDIT_PATH.is_postgres_password=$is_pg_password"
   if [ -n "$key_value" ]; then
-    jq "$LOCAL_FILE" attribute set "$HCLEDIT_PATH.value" "$key_value"
+    jq "$LOCAL_FILE" attribute set "$HCLEDIT_PATH.value=$key_value"
   fi
 done
 
