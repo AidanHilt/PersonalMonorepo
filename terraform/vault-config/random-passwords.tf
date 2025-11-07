7245eae40c890daa183dd81eff3874183d2749b9
Aidan Hilt <11202897+AidanHilt@users.noreply.github.com>
2025-11-06 22:35:33 -0500

    Grabbing some items we missed (#226)

--- terraform/vault-config/random-passwords.tf
+++ terraform/vault-config/random-passwords.tf
@@ -1,8 +1,8 @@
 resource "random_password" "generated" {
   for_each = merge([
-    for secret in local.secret_definitions : {
+    for secret_name, secret in local.secret_definitions : {
       for key, config in secret.data :
-        "${secret.name}-${key}" => merge(
+        "${secret_name}-${key}" => merge(
           {
             length  = lookup(config, "postgres_password", false) ? 64 : 32
             special = lookup(config, "postgres_password", false) ? true : false
