9a0d3f35716979df27be97c231beb3b4ef40aa97
aidan <aidanhilt2@gmail.com>
2025-11-06 20:09:47 -0500

    Nix commit

--- nix/mono-flake/modules/scripts/app-creator/app-creator-add-terraform-secret.nix
+++ nix/mono-flake/modules/scripts/app-creator/app-creator-add-terraform-secret.nix
@@ -19,17 +19,6 @@ show_help () {
   echo "OPTIONS:"
 }
 
-parse_args() {
-  for arg in "$@"; do
-    case "$arg" in
-      --secret-name=*) SECRET_NAME="‘''${arg#*=}" ;;
-      --secret-namespace=*) SECRET_NAMESPACE="‘''${arg#*=}" ;;
-      --secret-mount=*) SECRET_MOUNT="‘''${arg#*=}" ;;
-      --postgres-secret=*) POSTGRES_SECRET="true" ;;
-    esac
-  done
-}
-
 get_input() {
   local prompt="$1"
   local default="$2"
@@ -56,69 +45,83 @@ add_secret_key() {
   SECRET_KEYS+=("$key_name|$is_pg_password|$key_value")
 }
 
-main() {
-  parse_args "$@"
 
-  SECRET_NAME=""
-  SECRET_NAMESPACE=""
-  SECRET_MOUNT=""
-  POSTGRES_SECRET=false
+SECRET_NAME=""
+SECRET_NAMESPACE=""
+SECRET_MOUNT=""
+POSTGRES_SECRET=false
 
-  if [ -z "$SECRET_NAME" ]; then
-    SECRET_NAME="$(get_input "Enter secret name" "")"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --secret-name)
+      SECRET_NAME="$2"
+      shift 2
+      ;;
+      --secret-namespace)
+      SECRET_NAMESPACE="$2"
+      shift 2
+      ;;
+      --secret-mount)
+      SECRET_MOUNT="$2"
+      shift 2
+      ;;
+      --postgres-secret)
+      POSTGRES_SECRET="true"
+      shift 2
+      ;;
+
+
+if [ -z "$SECRET_NAME" ]; then
+  SECRET_NAME="$(get_input "Enter secret name" "")"
+fi
+
+if [ -z "$SECRET_NAMESPACE" ]; then
+  SECRET_NAMESPACE="$(get_input "Enter secret namespace" "")"
+fi
+
+if [ -z "$SECRET_MOUNT" ]; then
+  SECRET_MOUNT="$(get_input "Enter secret mount" "")"
+fi
+
+if [ -z "$POSTGRES_SECRET" ]; then
+  POSTGRES_SECRET="$(get_input "Does the secret contain postgres creds? (y/n)" "n")"
+fi
+
+print_debug "Collecting secret keys"
+SECRET_KEYS=()
+add_keys="$(get_input "Would you like to enter any secret keys? (y/n)" "n")"
+while [ "$add_keys" = "y" ]; do
+  add_secret_key
+  add_keys="$(get_input "Add another key? (y/n)" "n")"
+done
+
+LOCAL_FILE="''${PERSONAL_MONOREPO_LOCATION}/terraform/vault-config/locals.tf.json"
+print_debug "Updating locals.tf.json at $LOCAL_FILE"
+
+for entry in "‘''${SECRET_KEYS[@]}"; do
+  key_name="$(cut -d'|' -f1 <<< "$entry")"
+  is_pg_password="$(cut -d'|' -f2 <<< "$entry")"
+  key_value="$(cut -d'|' -f3 <<< "$entry")"
+  HCLEDIT_PATH=".locals.secret_definitions.‘''${SECRET_NAME}.data.‘''${key_name}"
+  jq "$LOCAL_FILE" "$HCLEDIT_PATH.is_postgres_password=$is_pg_password"
+  if [ -n "$key_value" ]; then
+    jq "$LOCAL_FILE" attribute set "$HCLEDIT_PATH.value" "$key_value"
   fi
+done
 
-  if [ -z "$SECRET_NAMESPACE" ]; then
-    SECRET_NAMESPACE="$(get_input "Enter secret namespace" "")"
-  fi
-
-  if [ -z "$SECRET_MOUNT" ]; then
-    SECRET_MOUNT="$(get_input "Enter secret mount" "")"
-  fi
-
-  if [ -z "$POSTGRES_SECRET" ]; then
-    POSTGRES_SECRET="$(get_input "Does the secret contain postgres creds? (y/n)" "n")"
-  fi
-
-  print_debug "Collecting secret keys"
-  SECRET_KEYS=()
-  add_keys="$(get_input "Would you like to enter any secret keys? (y/n)" "n")"
-  while [ "$add_keys" = "y" ]; do
-    add_secret_key
-    add_keys="$(get_input "Add another key? (y/n)" "n")"
-  done
-
-  LOCAL_FILE="''${PERSONAL_MONOREPO_LOCATION}/terraform/vault-config/locals.tf.json"
-  print_debug "Updating locals.tf.json at $LOCAL_FILE"
-
-  for entry in "‘''${SECRET_KEYS[@]}"; do
-    key_name="$(cut -d'|' -f1 <<< "$entry")"
-    is_pg_password="$(cut -d'|' -f2 <<< "$entry")"
-    key_value="$(cut -d'|' -f3 <<< "$entry")"
-    HCLEDIT_PATH=".locals.secret_definitions.‘''${SECRET_NAME}.data.‘''${key_name}"
-    jq "$LOCAL_FILE" "$HCLEDIT_PATH.is_postgres_password=$is_pg_password"
-    if [ -n "$key_value" ]; then
-      jq "$LOCAL_FILE" attribute set "$HCLEDIT_PATH.value" "$key_value"
-    fi
-  done
-
-  jq \
-    --arg name "$SECRET_NAME" \
-    --arg ns "$SECRET_NAMESPACE" \
-    --arg mount "$SECRET_MOUNT" \
-    --arg pg "$POSTGRES_SECRET" \
-    '
-    .locals[0].secret_definitions[$name].namespace = $ns
-    | .locals[0].secret_definitions[$name].mount = $mount
-    | .locals[0].secret_definitions[$name].postgres_secret = $pg
-    ' "$LOCAL_FILE" > tmp.json && mv tmp.json "$LOCAL_FILE"
+jq \
+  --arg name "$SECRET_NAME" \
+  --arg ns "$SECRET_NAMESPACE" \
+  --arg mount "$SECRET_MOUNT" \
+  --arg pg "$POSTGRES_SECRET" \
+  '
+  .locals[0].secret_definitions[$name].namespace = $ns
+  | .locals[0].secret_definitions[$name].mount = $mount
+  | .locals[0].secret_definitions[$name].postgres_secret = $pg
+  ' "$LOCAL_FILE" > tmp.json && mv tmp.json "$LOCAL_FILE"
 
 
-  print_status "Secret definition for $SECRET_NAME added to locals.tf.json"
-}
-
-main "$@"
-
+  print_status "Secret definition for $SECRET_NAME added to locals file"
 '';
 in
 
