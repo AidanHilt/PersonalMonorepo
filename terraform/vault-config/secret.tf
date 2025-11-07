2d60155b12c7035b56ecebdba35def0373447cd5
aidan <aidanhilt2@gmail.com>
2025-11-06 22:35:05 -0500

    Grabbing some items we missed

--- terraform/vault-config/secret.tf
+++ terraform/vault-config/secret.tf
@@ -1,24 +1,24 @@
 resource "vault_policy" "reader" {
-  for_each = { for secret in local.secret_definitions : secret.name => secret }
+  for_each = local.secret_definitions
 
-  name = try(each.value.auth.role_name, each.value.name)
+  name = try(each.value.auth.role_name, each.key)
 
   policy = <<EOT
-path "${each.value.mount}/data/${try(each.value.auth.path, each.value.name)}${try(each.value.auth.path, each.value.name) != "" ? "/*" : "*"}" {
+path "${each.value.mount}/data/${try(each.value.auth.path, each.key)}${try(each.value.auth.path, each.key) != "" ? "/*" : "*"}" {
   capabilities = ["read", "list"]
 }
-path "${each.value.mount}/${try(each.value.auth.path, each.value.name)}${try(each.value.auth.path, each.value.name) != "" ? "/*" : "*"}" {
+path "${each.value.mount}/${try(each.value.auth.path, each.key)}${try(each.value.auth.path, each.key) != "" ? "/*" : "*"}" {
   capabilities = ["read", "list"]
 }
 EOT
 }
 
 resource "vault_kubernetes_auth_backend_role" "reader" {
-  for_each = { for secret in local.secret_definitions : secret.name => secret }
+  for_each = local.secret_definitions
 
   backend                          = "kubernetes"
-  role_name                        = try(each.value.auth.role_name, each.value.name)
-  bound_service_account_names      = concat([lookup(each.value, "service_account", each.value.name)], lookup(each.value, "postgres_secret", false) ? ["postgres-cluster"] : [])
+  role_name                        = try(each.value.auth.role_name, each.key)
+  bound_service_account_names      = concat([lookup(each.value, "service_account", each.key)], lookup(each.value, "postgres_secret", false) ? ["postgres-cluster"] : [])
   bound_service_account_namespaces = concat([each.value.namespace], lookup(each.value, "postgres_secret", false) ? ["postgres"] : [])
   token_ttl                        = 3600
   token_policies                   = [vault_policy.reader[each.key].name]
@@ -26,17 +26,17 @@ resource "vault_kubernetes_auth_backend_role" "reader" {
 }
 
 resource "vault_kv_secret_v2" "config" {
-  for_each = { for secret in local.secret_definitions : secret.name => secret }
+  for_each = local.secret_definitions
 
   mount     = vault_mount.kv_mounts[each.value.mount].path
-  name      = try(each.value.path, "${each.value.name}/config")
+  name      = try(each.value.path, "${each.key}/config")
   data_json = jsonencode({
     for key, config in each.value.data : key => (
       lookup(config, "value", null) != null
         ? config.value
         : lookup(config, "readFromVars", false)
           ? config.static[lookup(config, "key_name", key)]
-          : random_password.generated["${lookup(config, "key_name", "${each.value.name}-${key}")}"].result
+          : random_password.generated["${lookup(config, "key_name", "${each.key}-${key}")}"].result
     )
   })
 }
