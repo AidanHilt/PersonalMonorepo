f6095ea151957da7a88c97c04e4e88f0b61e052d
aidan <aidanhilt2@gmail.com>
2025-11-09 10:42:31 -0500

    Nix commit

--- nix/mono-flake/modules/scripts/app-creator/default.nix
+++ nix/mono-flake/modules/scripts/app-creator/default.nix
@@ -1,7 +1,7 @@
 { inputs, globals, pkgs, machine-config, lib, ...}:
 {
  imports = [
-    ./app-creator-add-app.nix
+    ./app-creator-add-argocd-app.nix
     ./app-creator-add-postgres-config.nix
     ./app-creator-add-terraform-secret.nix
     ./app-creator-add-argocd-app.nix
