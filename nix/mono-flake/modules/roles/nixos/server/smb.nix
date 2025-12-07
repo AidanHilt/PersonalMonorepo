42c4cf4904573206488946b5f44d913c21287bcb
aidan <aidanhilt2@gmail.com>
2025-12-07 11:37:01 -0500

    Nix commit

---
+++ nix/mono-flake/modules/roles/nixos/server/smb.nix
@@ -0,0 +1,40 @@
+{ inputs, globals, pkgs, machine-config, lib, ...}:
+
+let
+
+in
+
+{
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+      "storagePool" = {
+        "path" = "/storagePool";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "aidan";
+        "force group" = "aidan";
+      };
+    };
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+}
