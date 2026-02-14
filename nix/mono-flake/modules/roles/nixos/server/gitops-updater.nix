cb5cd0789daabe117e577d28e6c26291f9ed943e
aidan <aidanhilt2@gmail.com>
2026-02-14 13:40:42 -0500

    Nix commit

--- nix/mono-flake/modules/roles/nixos/server/gitops-updater.nix
+++ nix/mono-flake/modules/roles/nixos/server/gitops-updater.nix
@@ -151,15 +151,15 @@ in
     repositorySubdir = "nix/mono-flake";
     remotes = [
       {
-        name = "origin";
-        url = "https://github.com/AidanHilt/PersonalMonorepo";
-        branches.main.name = "master";
-      }
-      {
         name = "local";
         url = "file://${repoPath}";
         branches.main.name = "HEAD";
         poller.period = 5;
+      }
+      {
+        name = "origin";
+        url = "https://github.com/AidanHilt/PersonalMonorepo";
+        branches.main.name = "master";
       }
     ];
   };
