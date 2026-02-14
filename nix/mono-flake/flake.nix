9d02a135e68893cbe3cd2d3abc8b7bda5f5a4988
aidan <aidanhilt2@gmail.com>
2026-02-14 00:24:53 -0500

    Nix commit

--- nix/mono-flake/flake.nix
+++ nix/mono-flake/flake.nix
@@ -58,10 +58,6 @@
       url = "github:lnl7/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
-
-    # Versioning stuff
-    nixpkgs-terragrunt.url = "github:nixos/nixpkgs/0024e310e51151ee27d710e1f2fdf5e9c293da0b";
-    nixpkgs-terraform.url = "github:nixos/nixpkgs/0472f3c9c5618c9675fc8dcc9e6ea484a5590d6d";
   };
 
   outputs = { self, nixpkgs, darwin, ... }@inputs:
@@ -70,7 +66,7 @@
 
       globals = {
         nixConfig = inputs.personalMonorepo + "/nix";
-        personalMonorepoBranch = "feat/external-user-1-final-retry";
+        personalMonorepoBranch = "master";
         personalMonorepoURL = "https://github.com/AidanHilt/PersonalMonorepo";
       };
 
