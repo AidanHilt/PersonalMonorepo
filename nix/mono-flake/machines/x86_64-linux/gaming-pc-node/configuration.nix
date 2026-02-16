a50f6e04986c87ba83019d30f1a1b65603f80632
aidan <aidanhilt2@gmail.com>
2026-02-15 19:04:41 -0500

    Nix commit

--- nix/mono-flake/machines/x86_64-linux/gaming-pc-node/configuration.nix
+++ nix/mono-flake/machines/x86_64-linux/gaming-pc-node/configuration.nix
@@ -23,4 +23,27 @@
   networking.hostId = "8425e349";
 
   hardware.nvidia-container-toolkit.enable = true;
+
+  virtualisation.containerd = {
+    settings = {
+      plugins = {
+        "io.containerd.grpc.v1.cri" = {
+          enable_cdi = true;
+          cdi_spec_dirs = ["/etc/cdi" "/var/run/cdi"];
+          containerd = {
+            default_runtime_name = "nvidia";
+            runtimes = {
+              nvidia = {
+                privileged_without_host_devices = false;
+                runtime_type = "io.containerd.runc.v2";
+                options = {
+                  BinaryName = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
 }
