09ad8872e329daa32ba4e99671b6ae5e9128f971
aidan <aidanhilt2@gmail.com>
2026-02-15 22:44:49 -0500

    Nix commit

--- nix/mono-flake/machines/x86_64-linux/gaming-pc-node/configuration.nix
+++ nix/mono-flake/machines/x86_64-linux/gaming-pc-node/configuration.nix
@@ -35,7 +35,7 @@
   services.rke2 = {
     extraFlags = [
       "--default-runtime=nvidia"
-      "--container-runtime-endpoint="
+      "--container-runtime-endpoint='unix:///run/containerd/containerd.sock'"
     ];
   };
 
