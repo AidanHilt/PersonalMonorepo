d49561e159938d49fd154825f41f0d556e3e57ed
aidan <aidanhilt2@gmail.com>
2026-02-21 13:36:27 -0500

    Nix commit

--- nix/mono-flake/machines/x86_64-linux/gaming-pc-node/configuration.nix
+++ nix/mono-flake/machines/x86_64-linux/gaming-pc-node/configuration.nix
@@ -64,7 +64,7 @@
             runtimes = {
               nvidia = {
                 privileged_without_host_devices = false;
-                runtime_type = "oci";
+                runtime_type = "io.containerd.runc.v2";
                 options = {
                   BinaryName = lib.mkForce "${pkgs.nvidia-container-toolkit.tools}/bin/nvidia-container-runtime";
                 };
