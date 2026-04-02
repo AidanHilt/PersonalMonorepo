7cf1c33af51c01ada07b8f1b432650e8b4f2ca04
aidan <aidanhilt2@gmail.com>
2026-04-01 20:38:25 -0400

    Nix commit

--- nix/mono-flake/machines/x86_64-linux/big-boi-desktop/configuration.nix
+++ nix/mono-flake/machines/x86_64-linux/big-boi-desktop/configuration.nix
@@ -24,7 +24,7 @@
     efiSupport = true;
     efiInstallAsRemovable = true;
     useOSProber = true;
-    devices = ["/dev/nvme1n1"];
+    device = "nodev";
   };
 
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
