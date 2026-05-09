72edc6315a30bc121a4f5c066c6d45495a6199a3
Aidan Hilt <11202897+AidanHilt@users.noreply.github.com>
2026-05-09 17:25:36 -0400

    Argocd commit (#265)
    * Argocd commit
    
    * Argocd commit
    
    * Argocd commit
    
    * Argocd commit
    
    * Argocd commit
    
    * Argocd commit
    
    * Nix commit
    
    * Nix commit
    
    * Argocd commit
    
    * Argocd commit
    
    * Argocd commit
    
    * Argocd commit

---
+++ nix/terraform/modules/vault/infra.nix
@@ -0,0 +1,19 @@
+{ libFunctions, lib, ... }:
+
+let
+  standalone = true;
+
+  vaultProvider = import ../../lib/vault-provider.nix {inherit lib; inherit standalone;};
+  kubernetesProvider = import ../../lib/kubernetes-provider.nix {inherit lib;};
+
+  secretsSet = lib.importJson ./secrets.json;
+
+  vaultSecrets = libFunctions.mkVaultSecrets secretsSet;
+in
+
+lib.mkMerge [
+  vaultProvider
+  kubernetesProvider
+  vaultSecrets
+]
+
