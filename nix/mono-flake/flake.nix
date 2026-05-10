ce2598536e780d43e343bd9bb7a685416fcdc87a
aidan <aidanhilt2@gmail.com>
2026-05-10 15:17:55 -0400

    Nix commit

--- nix/mono-flake/flake.nix
+++ nix/mono-flake/flake.nix
@@ -11,7 +11,7 @@
 
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+     # inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nur.url = "github:nix-community/nur";
