e54f61bca159f66dd144be32c6b13cef1b69ad50
aidan <aidanhilt2@gmail.com>
2026-06-07 10:34:46 -0400

    Nix commit

--- nix/mono-flake/lib-internal/packages.nix
+++ nix/mono-flake/lib-internal/packages.nix
@@ -38,6 +38,7 @@ in
           "test_available_connections_without_limit_per_host"
           "test_available_connections_no_limits"
           "test_connect_tunnel_connection_release"
+          "test_invalid_header_spacing"
         ];
       });
     })
