26a5347a2d4bce54042271398425cd6412d35711
ahilt <aidanhilt2@gmail.com>
2026-06-21 23:33:40 -0400

    Nix commit

--- nix/mono-flake/home-manager/modules/vscode.nix
+++ nix/mono-flake/home-manager/modules/vscode.nix
@@ -5,7 +5,7 @@ let
 
   # Unfortunately, some VSCode extensions(!) do not work on aarch64-linux, so let's read what platform we're on,
   # and only apply those if its safe
-  platformSpecificExtensions = if pkgs.system != "aarch64-linux" then with pkgs.vscode-marketplace;[ms-python.vscode-pylance ms-python.python ms-python.debugpy] else [];
+  platformSpecificExtensions = if pkgs.system != "aarch64-linux" then with pkgs.vscode-marketplace;[ms-python.vscode-pylance ms-python.python] else [];
 in
 
 {
