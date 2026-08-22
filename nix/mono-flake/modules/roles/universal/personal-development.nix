a326b18be99e571436449f30f26d96a1af758f7a
aidan <aidanhilt2@gmail.com>
2026-08-22 12:26:38 -0400

    Nix commit

--- nix/mono-flake/modules/roles/universal/personal-development.nix
+++ nix/mono-flake/modules/roles/universal/personal-development.nix
@@ -1,28 +1,26 @@
 { inputs, globals, pkgs, machine-config, lib, ...}:
 
 {
-  imports = [
-    ../../scripts/app-creator/default.nix
-    ../../scripts/argocd/default.nix
-    ../../scripts/context/default.nix
-    ../../scripts/custom-images/default.nix
-    ../../scripts/helm/default.nix
-    ../../scripts/keepass/default.nix
-    ../../scripts/kubernetes/default.nix
-    ../../scripts/mono-flake/default.nix
-    ../../scripts/nixos/default.nix
-    ../../scripts/system-tasks/default.nix
-    ../../scripts/vault/default.nix
-    ../../scripts/terragrunt/default.nix
-  ];
+  imports =
+    let
+      scriptsDir = ../../scripts;
+      entries = builtins.readDir scriptsDir;
+      dirNames = builtins.filter
+        (name: entries.${name} == "directory")
+        (builtins.attrNames entries);
+      candidatePaths = map
+        (name: scriptsDir + "/${name}/default.nix")
+        dirNames;
+    in
+      builtins.filter builtins.pathExists candidatePaths;
 
   environment.systemPackages = with pkgs; [
     act
     agenix
+    cocogitto
+    nss
     syncthing
     vault
     weechat
-    nss
-    cocogitto
   ];
 }
