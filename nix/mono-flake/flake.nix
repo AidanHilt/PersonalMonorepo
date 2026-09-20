501ef56b83b1277598d325b9000451b906754a75
aidan <aidanhilt2@gmail.com>
2026-09-19 20:19:57 -0400

    fix(deployment): Duh

--- nix/mono-flake/flake.nix
+++ nix/mono-flake/flake.nix
@@ -95,7 +95,7 @@
         inputs.nix-vscode-extensions.overlays.default
         inputs.nix-cachyos-kernel.overlays.default
         (final: prev: {
-          linuxPackages_7_0 = inputs.kernel70Pkgs.linuxPackages_latest;
+          linuxPackages_7_0 = kernel70Pkgs.linuxPackages_latest;
         })
       ];
 
