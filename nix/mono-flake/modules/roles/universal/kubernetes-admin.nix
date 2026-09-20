f4f8004c0289033cbe22ed1cdbc986081cbd0b42
aidan <aidanhilt2@gmail.com>
2026-09-19 22:23:58 -0400

    chore(deployment): sdfasdf

--- nix/mono-flake/modules/roles/universal/kubernetes-admin.nix
+++ nix/mono-flake/modules/roles/universal/kubernetes-admin.nix
@@ -173,7 +173,7 @@ in
     '')
   ];
 
-  age.secrets.kubeconfig = lib.mkIf machine-config.secretMachine or true {
+  age.secrets.kubeconfig = pkgs.lib.mkIf machine-config.secretMachine or true {
     file = ../../../secrets/kubeconfig.age;
     mode = "400";
     owner = "${machine-config.username}";
