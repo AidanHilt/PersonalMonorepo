42882f769e34bc87c35e542636fa8e5c71ef8187
aidan <aidanhilt2@gmail.com>
2026-09-22 23:32:05 -0400

    fix: Fixing IP address

--- nix/mono-flake/modules/roles/universal/_hosts.nix
+++ nix/mono-flake/modules/roles/universal/_hosts.nix
@@ -4,7 +4,7 @@
     # Personal Machines
     # =================
     "192.168.86.40" = ["big-boi-desktop.lan"];
-    "192.168.86.24" = ["devbox.lan"];
+    "192.168.86.41" = ["devbox.lan"];
 
     # ===================
     # 3-node prod cluster
